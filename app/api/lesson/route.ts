1d64c71e516627c4b2ea1d26ae350a642837633e
deribewsoftware <deribewsoftware@gmail.com>
2024-02-23 22:19:41 +0300

    updated

---
+++ app/api/lesson/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prismadb"
+export async function POST(req:Request) {
+  const body=await req.json();
+  const {chapterId,
+    title ,
+    videoUrl,
+    videoThumbnail,
+    content }=body;
+
+    if(!chapterId || !videoThumbnail || !content || !title || !videoUrl) {
+      return NextResponse.json({
+        status: false,
+        message:"Invalid  parameters"
+      });
+     }
+
+     const newLesson=await prisma.lesson.create({
+      data:{
+        chapterId:chapterId, 
+        title:title,
+         videoUrl:videoUrl, 
+         videoThumbnail:videoThumbnail, 
+         content:content}
+     })
+     return NextResponse.json(newLesson);
+  
+}
