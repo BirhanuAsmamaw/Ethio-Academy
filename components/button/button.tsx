ea9134b6017854f9eaba022178f535be0c41bee4
deribewsoftware <deribewsoftware@gmail.com>
2024-02-23 16:15:24 +0300

    updated

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -14,12 +14,12 @@ const Button:React.FC<ButtonProps> = ({outline,small,title,onClick,className,isD
   className={`
   text-white 
  
-  disabled:bg-opacity-80 
+  disabled:opacity-75
  
   focus:ring-4
    focus:ring-blue-300 
    font-medium 
-   rounded-lg 
+   rounded-[5px]
    text-sm 
    px-5 
    py-2.5
