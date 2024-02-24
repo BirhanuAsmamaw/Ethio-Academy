0a95a32e763a4641f779e0e57376f5d88e0abd9e
deribewsoftware <deribewsoftware@gmail.com>
2024-02-24 13:08:14 +0300

    updated

--- app/course/[courseId]/[chapterId]/[lessonId]/page.tsx
+++ app/course/[courseId]/[chapterId]/[lessonId]/page.tsx
@@ -20,7 +20,10 @@ console.log("lessons data",lesson);
   <Navbar/>
   <div className="flex justify-center py-10 px-2">
 
-<div className="w-full md:w-10/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 flex flex-col gap-10  pt-10">
+<div className="px-2 w-full md:w-10/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 flex flex-col gap-10  pt-10 
+border-x-[1.5]
+dark:border-gray-700
+ border-gray-200">
    <h1 className="text-lg font-medium">{lesson.title}</h1>
    <div>
     <video
@@ -36,6 +39,7 @@ console.log("lessons data",lesson);
       <div className="p-4">
       <div className="" dangerouslySetInnerHTML={{ __html: lesson.content}}></div>
       </div>
+      <div className="">{lesson.chapter.course.subject}</div>
   </div>
   {/* course contents lits for above md */}
 {/* {lesson.chapter&&<div className="lg:fixed right-20 flex lg:h-screen items-center justify-center  ">
