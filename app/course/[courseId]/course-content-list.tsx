1ef23c1ca4511b34c2337f44472d6f92d0731e1e
deribewsoftware <deribewsoftware@gmail.com>
2024-02-24 11:57:36 +0300

    updated

--- app/course/[courseId]/course-content-list.tsx
+++ app/course/[courseId]/course-content-list.tsx
@@ -12,12 +12,13 @@ const CourseContentList = () => {
       <div className=" md:px-2 md:px-10  ">
 
       <div className="
+      text-sm
         border-b-[1.3px]
         flex justify-between
         p-3
         ">
           <div className="flex gap-3 ">
-          <div className="mt-1"><BsBarChart/></div>
+          <div className="mt-1"><BsBarChart size={15}/></div>
             <p>Level</p>
           </div>
           <div className="text-slate-400">Grade 12</div>
@@ -28,9 +29,10 @@ const CourseContentList = () => {
         border-b-[1.3px]
         flex justify-between
         p-3
+        text-sm
         ">
           <div className="flex gap-3 ">
-          <div className="mt-1"><LuBookMinus/></div>
+          <div className="mt-1"><LuBookMinus size={15}/></div>
             <p>Lessons</p>
           </div>
           <div className="text-slate-400">133</div>
@@ -38,12 +40,13 @@ const CourseContentList = () => {
         </div>
 
         <div className="
+        text-sm
         border-b-[1.3px]
         flex justify-between
         p-3
         ">
           <div className="flex gap-3 ">
-            <div className="mt-1"><IoTimeOutline/></div>
+            <div className="mt-1"><IoTimeOutline size={15}/></div>
             <p>Duration</p>
 
           </div>
@@ -55,9 +58,10 @@ const CourseContentList = () => {
         border-b-[1.3px]
         flex justify-between
         p-3
+        text-sm
         ">
           <div className="flex gap-3 ">
-          <div className="mt-1"><PiCertificateThin/></div>
+          <div className="mt-1"><PiCertificateThin size={15}/></div>
             <p>Certifications</p>
           </div>
           <div className="text-slate-400">Yes</div>
