897cf66142dc62962c2bf57e22adf7c3694a3311
deribewsoftware <deribewsoftware@gmail.com>
2024-02-24 12:03:03 +0300

    updated

--- app/course/[courseId]/[chapterId]/[lessonId]/page.tsx
+++ app/course/[courseId]/[chapterId]/[lessonId]/page.tsx
@@ -1,5 +1,7 @@
 import { getLessonById } from "@/actions/getLessonById";
 import LessonClient from "./lessonClient";
+import CourseSceleton from "../courseSceleton";
+import Navbar from "@/components/navbar/Navbar";
 
 interface IParams{
   lessonId: string
@@ -10,12 +12,28 @@ const LessonPage = async({params}:{params:IParams}) => {
   
 
   if(!lesson){
-    return <div className="">No lesson </div>
+    return <div className=""><CourseSceleton/> </div>
   }
 
-  return ( <div className="">
+  return (<>
+  <Navbar/>
+   <div className="flex py-10 ">
    <h1 className="text-lg font-medium">{lesson.title}</h1>
-  </div> );
+   <div>
+    <video
+        className="w-full rounded-lg shadow-lg"
+        controls
+        poster={lesson.videoThumbnail || ''}
+      >
+        <source src={lesson.videoUrl || ''} type="video/mp4"  />
+        Your browser does not support the video tag.
+      </video></div>
+
+
+      <div className="p-4">
+      <div className="" dangerouslySetInnerHTML={{ __html: lesson.content}}></div>
+      </div>
+  </div></> );
 }
  
 export default LessonPage;
