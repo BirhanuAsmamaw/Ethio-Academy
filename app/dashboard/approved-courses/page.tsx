34d5f65d586039c366fe811f1c5ee4ece6433ea7
deribewsoftware <deribewsoftware@gmail.com>
2024-02-25 17:00:55 +0300

    updated

--- app/dashboard/approved-courses/page.tsx
+++ app/dashboard/approved-courses/page.tsx
@@ -4,7 +4,7 @@ import { ApprovedCoursesClient } from "./approvedCoursesClient";
 const ApprovedCourses = async() => {
   const approvedCourses=await getAllPayments();
 
-  return ( <div className="">
+  return ( <div className="py-10">
    <ApprovedCoursesClient coursePayments={approvedCourses||[]}/>
   </div> );
 }
