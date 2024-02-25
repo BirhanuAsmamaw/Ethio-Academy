a58363e8f17857cbca16fc746a0ca7478185bbf5
deribewsoftware <deribewsoftware@gmail.com>
2024-02-25 22:47:03 +0300

    updated

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -3,14 +3,14 @@ import { CiSearch } from "react-icons/ci";
 const Search = () => {
   return ( <>
     <div className="hidden md:block  ">
-   <div className="flex w-full md:w-md bg-white dark:bg-gray-700  border rounded-[8px] border-gray-200 dark:border-gray-800 overflow-hidden ">
+   <div className="flex w-full  bg-white dark:bg-gray-800  border rounded-[8px] border-gray-200 dark:border-gray-700 overflow-hidden ">
      <CiSearch size={30} className="pt-1"/> 
      <input type="search" className="bg-white dark:bg-gray-700  w-full border-none outline-none px-2 py-1"/></div>
    </div>
    <div className="md:hidden relative">
     <button><CiSearch size={30} className=""/></button>
     <div className="absolute top-13 w-full right-2 left-2">
-    <div className=" dark:border-gray-800 bg-white dark:bg-gray-700  shadow-lg flex w-[300px]  border rounded-[8px] border-gray-200 overflow-hidden ">
+    <div className=" dark:border-gray-700 bg-white dark:bg-gray-800  shadow-lg flex w-[300px]  border rounded-[8px] border-gray-200 overflow-hidden ">
     <CiSearch size={24} className="pt-1"/> 
     <input type="search" 
     className=" bg-white dark:bg-gray-700  w-full border-none outline-none px-2 py-1"/>
