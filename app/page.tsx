a1e6e65874f0520c4160b3a0ea6606d3102dc5e9
deribewsoftware <deribewsoftware@gmail.com>
2024-02-25 23:40:43 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -92,10 +92,10 @@ const courses=await getCourses()
 <h1 className='w-full text-xl md:text-4xl font-semibold border-b-2 border-double  p-2 dark:text-gray-300 border-gray-200 dark:border-gray-700 pl-4'>What to expect from Ethio Exams Academy courses</h1>
 
 
-<Accordion type="single" collapsible className="w-full p-1 bg-white dark:bg-gray-800" >
+<Accordion type="single" collapsible className="w-full p-1 " >
 
       {AboutAcademy.map((about:any,index:number) =>{
-        return <AccordionItem key={index} value={`${index}`}className="border   border-slate-200 dark:border-gray-600 px-2 md:px-6 m-2 rounded-[6px] ">
+        return <AccordionItem key={index} value={`${index}`}className="border bg-white dark:bg-gray-800  border-slate-200 dark:border-gray-600 px-2 md:px-6 m-2 rounded-[6px] ">
         <AccordionTrigger className="hover:no-underline" ><div className="flex gap-2  text-captalize  ">
          <p className="h-5 w-5 rounded-full bg-green-400  items-center text-center text-sm text-black">{index+1}</p>
          <p className="text-base">{about.title}</p>
