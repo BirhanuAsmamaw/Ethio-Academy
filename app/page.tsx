f9a61d6b867a05e9b4b37b247e8ed98cc44464ec
deribewsoftware <deribewsoftware@gmail.com>
2024-02-25 23:08:57 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ const courses=await getCourses()
    
     <main className='md:flex md:justify-center w-full pb-10 overflow-hidden'>
     <div id="courseslist" className="flex flex-col gap-10 overflow-hidden">
-      <h1 className='w-full text-xl md:text-4xl font-semibold border-b pl-4'>Most common Courses</h1>
+      <h1 className='w-full text-xl md:text-4xl font-semibold border-b border-double p-2 dark:text-gray-300 border-gray-200 dark:border-gray-700 pl-4'>Most common Courses</h1>
 
   
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-8 p-4 md:px-10'>
@@ -66,7 +66,7 @@ const courses=await getCourses()
 
 
 
-    <h1 className='w-full text-2xl md:text-4xl font-semibold border-b mt-10 pl-4'>New Coming  Courses</h1>
+    <h1 className='w-full text-xl md:text-4xl font-semibold border-b border-double p-2 dark:text-gray-300 border-gray-200 dark:border-gray-700 pl-4'>New Coming  Courses</h1>
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-8 p-4 md:px-10'>
 
         {courses?.map((course,index)=>{
