033a1106b8a270d97c1cf0955b29add662aadb0c
deribewsoftware <deribewsoftware@gmail.com>
2024-02-25 22:45:06 +0300

    updated

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -11,7 +11,7 @@ const Search = () => {
     <button><CiSearch size={30} className=""/></button>
     <div className="absolute top-13 w-full right-2 left-2">
     <div className=" dark:border-gray-800 bg-white dark:bg-gray-700  shadow-lg flex w-[300px]  border rounded-[8px] border-gray-200 overflow-hidden ">
-    <CiSearch size={30} className="pt-1"/> 
+    <CiSearch size={24} className="pt-1"/> 
     <input type="search" 
     className=" bg-white dark:bg-gray-700  w-full border-none outline-none px-2 py-1"/>
    </div>
