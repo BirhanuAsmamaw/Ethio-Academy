9ec49a4cf0472063d7bf641d09f78b84abb51475
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 22:58:14 +0300

    updated

--- app/search/searchClient.tsx
+++ app/search/searchClient.tsx
@@ -1,12 +1,34 @@
-"use client"
-
+import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const SearchClient = () => {
+  const searchParam = useSearchParams();
+  const searchQueryData = searchParam?.get("q");
+  const [courses, setCourses] = useState<any[]>([]);
 
-  const searchParam=useSearchParams();
-  const searchQueryData=searchParam?.get("q")
-  return (<div className="">{searchQueryData}</div>  );
-}
- 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/api/course/coursesearch?subject=${searchQueryData}`);
+        // Assuming the data you want is in response.data, modify accordingly
+        setCourses(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle error appropriately (e.g., show an error message)
+      }
+    };
+
+    if (searchQueryData) {
+      fetchData();
+    }
+  }, [searchQueryData]);
+
+  return <div className="">
+    {courses.map((course) =>{
+      return <div key={course.id}>{course.subject}</div>
+    })}
+  </div>;
+};
+
 export default SearchClient;
