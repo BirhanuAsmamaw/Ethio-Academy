a5ca203fb22b69a9cb0310c767c49e5d2ed1a8de
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 16:15:36 +0300

    updated

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -18,7 +18,7 @@ const Search = () => {
      <input onChange={(event)=>setSearchQuery(event.target.value)} type="search" className="focus:shadow-md bg-white dark:bg-gray-800  w-full border-none outline-none px-2 py-1"/>
      </div>
      <div className="absolute top-13 w-full bg-white dark:bg-gray-800">
-      <div className="flex flex-col w-full gap-2 p-2">
+      <div className="flex flex-col w-full gap-2 p-2 shadow-xl rounded-b-[10px] shadow-gray-100">
         <p className="text-xs">Biology Grade 12</p>
         <p className="text-xs">Biology Grade 12</p>
         <p className="text-xs">Biology Grade 12</p>
@@ -28,9 +28,9 @@ const Search = () => {
       </div>
      </div>
    </div>
-   <div className="md:hidden  bg-blue-600 relative w-full">
+   <div className="md:hidden  relative w-full">
     <button><CiSearch size={30} className="pt-2 text-gray-500 dark:text-gray-400"/></button>
-    <div className="absolute top-16 w-[400px] left-2 right-10 ">
+    <div className="absolute top-16 w-[300px] left-0 right-10 bg-rose-400">
     <div  className=" dark:border-gray-700 bg-white dark:bg-gray-800  shadow-lg flex w-full  border rounded-[8px] border-gray-200 overflow-hidden ">
     <CiSearch size={30} className="pt-1"/> 
     <input  onChange={(event)=>setSearchQuery(event.target.value)} type="search" 
