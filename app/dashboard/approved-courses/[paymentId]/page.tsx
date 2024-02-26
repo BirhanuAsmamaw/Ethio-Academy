84d4dc71dc130e235a7d5e93b2fc46bdba153bee
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 10:26:41 +0300

    updated

--- app/dashboard/approved-courses/[paymentId]/page.tsx
+++ app/dashboard/approved-courses/[paymentId]/page.tsx
@@ -11,25 +11,38 @@ const PaymentDetail = async({params}:{params:{paymentId:string}}) => {
   return ( <div className="w-full flex flex-col justify-center items-center gap-10">
 
           <div className="">
-            <Image height={400} width={370} src={payment.recit|| ''} alt="payment reciept"/>
+            <Image height={500} width={400} src={payment.recit|| ''} alt="payment reciept"/>
           </div>
           <div className="w-full flex gap-10">
-            <div>
-            <p>TransactionId</p>
-            <p>{payment.transaction}</p>
-            </div>
+          <table className="table-fixed">
+  
+  <tbody>
+    <tr>
+      <th>Bank</th>
+      <td>{payment.bank}</td>
+    </tr>
+    <tr>
+      <th>TransactionId</th>
+      <td>{payment.transaction}</td>
+      
+    </tr>
+    <tr>
+    <th>Price</th>
+      <td>{payment.totalPrice}</td>
+     
+    </tr>
+  </tbody>
+  <tfoot>
+  <div className="flex justify-end py-2 px-4">
+            <button className="border rounded-[10px] bg-blue-600 hover:bg-blue-500 transition px-2 py-1">Approve</button>
+          </div>
+  </tfoot>
+</table>
+          </div>
 
-            <div>
-            <p>Bank Type</p>
-            <p>{payment.bank}</p>
-            </div>
-          </div>
-          <div className="">
-            <p>{payment.totalPrice}</p>
-          </div>
-          <div className="">
-            <button className="border rounded-[10px] px-2 py-1">Approve</button>
-          </div>
+
+
+          
 
 
   </div> );
