ed2e8dab5154b4b6d298341cbd9df4c9e1f8921c
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 15:34:26 +0300

    updated

---
+++ app/search/searchClient.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useSearchParams } from "next/navigation";
+
+
+const SearchClient = () => {
+
+  const searchParams = useSearchParams();
+
+  return (<div className="w-full flex min-h-screen justify-center items-center">
+    <div className="w-full p-2 md:w-10/12 lg:w-8/12 xl:w-6/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 flex-col gap-10 justify-center items-center">
+      
+      <div className="h-17 w-64 bg-gray-800">card1</div>
+      <div className="h-17 w-64 bg-gray-800">card1</div>
+      
+
+    </div>
+  </div>  );
+}
+ 
+export default SearchClient;
