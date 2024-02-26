99e2d4641cf82189983e0367d6f5c490cac03ffd
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 13:44:40 +0300

    updated

---
+++ app/settings/page.tsx
@@ -0,0 +1,7 @@
+const SettingPage = () => {
+  return ( <div className="min-h-screen w-full flex justify-center items-center">
+    
+  </div> );
+}
+ 
+export default SettingPage;
---
+++ app/settings/settingsClient.tsx
@@ -0,0 +1,34 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+
+
+
+const SettingsClient = () => {
+  return ( <Tabs defaultValue="account" className="w-[400px]">
+  <TabsList className="grid w-full grid-cols-2">
+    <TabsTrigger value="account">Account</TabsTrigger>
+    <TabsTrigger value="password">Password</TabsTrigger>
+  </TabsList>
+  <TabsContent value="account">
+    <div className="flex flex-col gap-4 w-full">
+      <h1 className="text-lg font-semibold">Edit Your Account</h1>
+      <p>upload profile</p>
+      <p>name</p>
+      <p>email</p>
+    </div>
+  </TabsContent>
+
+
+
+  <TabsContent value="password">
+  <div className="flex flex-col gap-4 w-full">
+      <h1 className="text-lg font-semibold">Edit Your Password</h1>
+      <p>upload profile</p>
+      <p>old Password</p>
+      <p>new password</p>
+    </div>
+  </TabsContent>
+</Tabs> );
+}
+ 
+export default SettingsClient;
