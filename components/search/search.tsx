e84355f00bbdd983298b4083ebd3d336e6d1f8d9
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 20:53:18 +0300

    updated

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -5,44 +5,31 @@ import Link from "next/link";
 import { IoIosSearch } from "react-icons/io";
 import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
+import { useRouter } from "next/navigation";
 interface SearchProps{
   courses:CourseType[] | any[];
 }
 const Search:React.FC<SearchProps> = ({courses}) => {
 
-  
+  const router=useRouter();
 
   const [searchQuery,setSearchQuery] = useState<string|null>(null);
+  const encodedSearchQuery=encodeURIComponent(searchQuery||"");
+  const onHandleSubmit = (event:React.FormEvent)=>{
+    event.preventDefault();
+     
+    router.push(`/search?q=${encodedSearchQuery}`);
 
-  const filteredcourse=courses.filter((course)=>course.subject.includes(searchQuery||''))
+  }
 
   
   return ( <>
     <div className="hidden md:block w-full relative ">
-   <div  className="flex w-full  bg-white dark:bg-gray-800  border rounded-[8px] border-gray-200 dark:border-gray-700 overflow-hidden ">
+   <form onSubmit={onHandleSubmit}  className="flex w-full  bg-white dark:bg-gray-800  border rounded-[8px] border-gray-200 dark:border-gray-700 overflow-hidden ">
      <CiSearch size={30} className="pt-1"/> 
-     <input onChange={(event)=>setSearchQuery(event.target.value)} type="search" className="focus:shadow-md bg-white dark:bg-gray-800  w-full border-none outline-none px-2 py-1"/>
-     </div>
-     <div className={`absolute top-13
-      w-full bg-white
-       dark:bg-gray-800 
-      shadow-xl
-       rounded-b-[10px]
-      shadow-gray-100
-      dark:shadow-gray-900
-      ${!searchQuery&&'hidden'}
-      `}>
-
-      <div className="flex flex-col w-full gap-2 p-2">
-     {
-      filteredcourse.map((c) =>{
-        return <Link key={c.id} href={`/course/${c.id}`} className="no-underline p-1 hover:bg-gray-100  hover:dark:bg-gray-700 text-xs text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition ">
-          {c.subject}
-          </Link>
-      })
-     }
-     </div>
-   </div>
+     <input value={searchQuery||''} onChange={(event)=>setSearchQuery(event.target.value)} type="search" className="focus:shadow-md bg-white dark:bg-gray-800  w-full border-none outline-none px-2 py-1"/>
+     </form>
+   
    </div>
    <div className="md:hidden   w-full">
     <button><IoIosSearch size={30}  className="pt-2 no-underline text-gray-500 dark:text-gray-400 font-medium  hover:dark:text-green-400 hover:text-rose-400 transition duration-300 font-medium"/></button>
