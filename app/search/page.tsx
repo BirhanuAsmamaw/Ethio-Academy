402cd1b83182993ca161fa1817df27dca980de07
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 21:01:18 +0300

    updated

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,19 @@
+"use client"
+
+import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
-import SearchClient from "./searchClient";
+
 
 const SearchPage = () => {
-  return (<Suspense><SearchClient/></Suspense> );
+
+  const searchParam=useSearchParams();
+const searchQueryData=searchParam? searchParam.get("q"):null;
+
+  return (<Suspense>
+<div className="">
+<h1>{searchQueryData}</h1>
+</div>
+  </Suspense> );
 }
  
 export default SearchPage;
