40a62de87669335bc2879f5d9825545aef32656e
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 13:45:15 +0300

    updated

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,8 @@
+import SettingsClient from "./settingsClient";
+
 const SettingPage = () => {
   return ( <div className="min-h-screen w-full flex justify-center items-center">
-    
+    <SettingsClient/>
   </div> );
 }
  
