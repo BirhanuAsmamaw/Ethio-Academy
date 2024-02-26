16502decca2e61317c9b145fcfbf0abd8a033146
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 13:17:50 +0300

    updated

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 import Banner from "../../public/banner.png"
 import { Avatar, AvatarGroup } from "@mui/material";
 import { students } from "@/lib/students";
-import Search from "../search/search";
-import { BsExclamationLg } from "react-icons/bs";
+
 
 const Hero = () => {
   
   return ( <div className="w-full flex min-h-screen justify-center md:justify-around items-center flex-wrap px-4 items-center
     dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center"
   >
+    <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
     {/* content */}
     <div className="pt-10 md:pt-0 flex flex-col gap-20 justify-center items-center  md:w-2/3">
       <div className="flex flex-col gap-6">
