e667021ba7afd0f1b221a0c4447dffdd3d2bf186
deribewsoftware <deribewsoftware@gmail.com>
2024-02-26 15:21:56 +0300

    updated

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 const Search = () => {
 
   const searchParams = useSearchParams();
-  const searchQuery =searchParams? searchParams.get("q"):null;
+
   return (<div className="w-full flex min-h-screen justify-center items-center">
     <div className="w-full p-2 md:w-10/12 lg:w-8/12 xl:w-6/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 flex-col gap-10 justify-center items-center">
       
