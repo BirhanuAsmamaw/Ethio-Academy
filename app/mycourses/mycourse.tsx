b69782976c9c75de720559e5cf549887bb8d6642
deribewsoftware <deribewsoftware@gmail.com>
2024-02-27 17:08:11 +0300

    updated

--- app/mycourses/mycourse.tsx
+++ app/mycourses/mycourse.tsx
@@ -1,13 +1,11 @@
 
-import { courses } from "@/lib/courses";
-import { usersData } from "@/lib/users";
 
-const MyCourseClient = () => {
-const id="1"
-    const user=usersData.filter((user) => user.id===id);
-    const enrolledCourses = user
-    ? courses.filter((course) => course.users.some((u) => u.id === id))
-    : [];
+import Link from "next/link";
+interface MyCourseClientProps{
+    user:any;
+}
+const MyCourseClient:React.FC<MyCourseClientProps> = ({user}) => {
+
   return ( 
 
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -17,55 +15,23 @@ const id="1"
                     <th scope="col" className="px-6 py-3">
                         Course name
                     </th>
+                    
                     <th scope="col" className="px-6 py-3">
-                        Percent
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        Status
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        Completion
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        <span className="sr-only">Edit</span>
+                        <span className="sr-only">Learn</span>
                     </th>
                 </tr>
             </thead>
             <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                       Biology Grade 12 Course
-                    </th>
-                    <td className="px-6 py-4">
-                        78%
-                    </td>
-                    <td className="px-6 py-4">
-                        Approved
-                    </td>
-                    <td className="px-6 py-4">
-                        Incomplete
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    </td>
-                </tr>
+               
 
-{enrolledCourses.map((course)=>{
+{user.courses.map((course:any)=>{
     return <tr key={course.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
        {course.subject}
     </th>
-    <td className="px-6 py-4">
-        {course.percent}%
-    </td>
-    <td className="px-6 py-4">
-        Approved
-    </td>
-    <td className="px-6 py-4">
-        Incomplete
-    </td>
+    
     <td className="px-6 py-4 text-right">
-        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        <Link href={`/course/${course.id}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Start</Link>
     </td>
 </tr>
 
--- app/mycourses/page.tsx
+++ app/mycourses/page.tsx
@@ -7,15 +7,24 @@ import { getCurrentUser } from "@/actions/users/currentUser";
 const MyCourses = async() => {
   const user=await getCurrentUser();
   console.log(user?.payedCourses);
+
+  const myCourse=user?.payedCourses.map((payedCourse) =>{
+    const course=payedCourse.courses;
+    return [...course ,]
+  })
+  const myData={
+    name:user?.name,
+    email:user?.email,
+    courses:myCourse};
   return ( <>
   <Navbar/>
   <div className="min-h-screen flex flex-col justify-center ">
     
 <div className="flex gap-10 flex-wrap  justify-center">
   <div className="">
-    <UserProfile/>
+    <UserProfile user={user}/>
   </div>
-  <div className="overflow-x-auto p-2"><MyCourseClient/></div>
+  <div className="overflow-x-auto p-2"><MyCourseClient user={myData}/></div>
 </div>
     </div>
   </> );
