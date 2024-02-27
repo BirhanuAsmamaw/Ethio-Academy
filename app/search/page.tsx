6095b36a35b440019235c65658291db24a3c594a
deribewsoftware <deribewsoftware@gmail.com>
2024-02-27 10:35:51 +0300

    updated

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,18 @@
 import { Suspense } from "react";
 import SearchClient from "./searchClient";
 import Spinning from "@/components/spinning";
+import Navbar from "@/components/navbar/Navbar";
 
 
 const SearchPage =async () => {
-  return (<Suspense fallback={<div className="h-screen w-full flex justify-center items-center flex gap-1 text-xl">
+  return (<>
+  <Navbar/>
+  <Suspense fallback={<div className="h-screen w-full flex justify-center items-center flex gap-1 text-xl">
     <Spinning/>
     <p>Searching...</p>
-  </div>}><SearchClient/></Suspense> );
+  </div>}>
+    <SearchClient/>
+    </Suspense> </>);
 }
  
 export default SearchPage;
