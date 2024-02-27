1e5eacea2ebee769a3b7f75bc5d7192abd5f4772
deribewsoftware <deribewsoftware@gmail.com>
2024-02-27 21:16:03 +0300

    updated

--- app/mycourses/page.tsx
+++ app/mycourses/page.tsx
@@ -13,8 +13,12 @@ const user=await getCurrentUser();
  
   return ( <>
   <Navbar/>
- 
-  <div className="overflow-x-auto p-2">
+ <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-20">
+
+<div className="w-64 h-72 border rounded-[10px]"></div>
+
+
+  <div className="overflow-x-auto p-2 ">
   <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -48,7 +52,7 @@ const user=await getCurrentUser();
         </table>
     </div>
   </div>
-
+  </div>
     
   </> );
 }
