6c44706d1e2f4bcee6b79f7c8c9577a6b992e672
deribewsoftware <deribewsoftware@gmail.com>
2024-02-27 20:09:39 +0300

    updated

--- app/mycourses/page.tsx
+++ app/mycourses/page.tsx
@@ -7,12 +7,12 @@ import { getCurrentUser } from "@/actions/users/currentUser";
 const MyCourses = async() => {
 
 
-const user=getCurrentUser();
+const user=await getCurrentUser();
  
   return ( <>
   <Navbar/>
  
-  <div className="overflow-x-auto p-2">my courses</div>
+  <div className="overflow-x-auto p-2">my courses deri</div>
 
     
   </> );
