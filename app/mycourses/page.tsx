f395ddf644540cd4cedf44e6c9fb43b6c35700d3
deribewsoftware <deribewsoftware@gmail.com>
2024-02-27 19:42:33 +0300

    updated

--- app/mycourses/page.tsx
+++ app/mycourses/page.tsx
@@ -1,26 +1,17 @@
 import Navbar from "@/components/navbar/Navbar";
 
 
-import { getCurrentUser } from "@/actions/users/currentUser";
+const MyCourses = () => {
 
-
-const MyCourses = async() => {
-  const user=await getCurrentUser();
 
 
  
   return ( <>
   <Navbar/>
-  <div className="min-h-screen py-10 flex flex-col justify-center ">
-    
-<div className="flex gap-10 flex-wrap  justify-center">
-  {/* <div className="">
-    <UserProfile user={user}/>
-    deribew
-  </div> */}
+ 
   <div className="overflow-x-auto p-2">my courses</div>
-</div>
-    </div>
+
+    
   </> );
 }
  
