6251c1cd096bd9189a5422814767b464229cb2d4
deribewsoftware <deribewsoftware@gmail.com>
2024-02-28 17:38:19 +0300

    updated

--- app/dashboard/list-courses/[courseId]/chapter/[chapterId]/lesson/page.tsx
+++ app/dashboard/list-courses/[courseId]/chapter/[chapterId]/lesson/page.tsx
@@ -270,7 +270,14 @@ disabled={isDisabled}
 
   <div className="p-4" dangerouslySetInnerHTML={{__html:dataValue.content}}>
   </div>
-<div className="w-full py-10 px-4 flex justify-end">
+<div className="w-full py-10 px-4 gap-4 flex justify-end">
+
+<button onClick={onNext} type="button" className="text-white bg-blue-700 hover:bg-blue-800 
+focus:ring-4 focus:outline-none focus:ring-blue-300 
+disabled:bg-blue-400 disabled:dark:bg-blue-500 disabled:cursor-not-allowed
+ font-medium rounded-lg text-sm px-5 py-2.5 text-center" 
+disabled={isDisabled}
+>Back</button>
             <Button 
             isDisabled={isLoading}
             onClick={handleSubmit(onSubmit)}
