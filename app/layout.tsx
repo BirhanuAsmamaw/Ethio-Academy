7f90cec61b3095fa0e60e70256b585ac98c5107e
deribewsoftware <deribewsoftware@gmail.com>
2024-03-01 00:34:13 +0300

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next'
-import { Noto_Sans} from 'next/font/google'
+import {IBM_Plex_Sans} from 'next/font/google'
 import './globals.css'
 import AppProvider from '@/providers/appProvider'
 import Footer from '@/components/footer/footer'
 import { Toaster } from 'react-hot-toast'
 
 
-const josefin= Noto_Sans({ subsets: ['latin'] ,weight:['400', '500','600','700']})
+const IBM_plex_sans= IBM_Plex_Sans({ subsets: ['latin'] ,weight:['400', '500','600','700']})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} bg-gray-100 dark:text-gray-200 overflow-x-hidden mx-auto dark:bg-gray-900 `}>
+      <body className={`${IBM_plex_sans.className} bg-gray-100 dark:text-gray-200 overflow-x-hidden mx-auto dark:bg-gray-900 `}>
       <Toaster
   position="top-center"
   reverseOrder={false}
