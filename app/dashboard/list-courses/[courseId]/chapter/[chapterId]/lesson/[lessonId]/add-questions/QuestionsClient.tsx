0254e6bc24f1ebff1eb643f903da3e20aaa41547
deribewsoftware <deribewsoftware@gmail.com>
2024-03-01 11:20:34 +0300

    updated

--- app/dashboard/list-courses/[courseId]/chapter/[chapterId]/lesson/[lessonId]/add-questions/QuestionsClient.tsx
+++ app/dashboard/list-courses/[courseId]/chapter/[chapterId]/lesson/[lessonId]/add-questions/QuestionsClient.tsx
@@ -136,7 +136,7 @@ const onSubmit=() => {
  console.log("Question",questions);
  
 };
-  return (  <div className={`bg-white dark:bg-gray-800 pb-10  mb-10 min-h-screen flex flex-col items-center gap-6 w-full`}>
+  return (  <div className={`bg-white dark:bg-gray-800 pb-10 px-3 mb-10 min-h-screen flex flex-col items-center gap-6 w-full`}>
   
      <div className="p-4">
       <h5 className="text-lg">Write Questions of <span className="text-green-600 dark:text-green-400">{lesson.title}</span> in <span className="text-rose-600 dark:text-rose-400">{lesson.chapter.title} </span>of <span className="text-blue-600 dark:text-blue-400">{lesson.chapter.course.subject}</span></h5>
