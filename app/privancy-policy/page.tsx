dd8856effcf5aea43e067971c3dd41d8ce9b2d08
deribewsoftware <deribewsoftware@gmail.com>
2024-03-02 20:29:39 +0300

    updated

--- app/privancy-policy/page.tsx
+++ app/privancy-policy/page.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header";
+import MainLayout from "@/components/layouts/mainLayout";
+import SubLayout from "@/components/layouts/subLayout";
 
 const PrivancyPolicy = () => {
   return ( <>
@@ -7,7 +9,10 @@ const PrivancyPolicy = () => {
     description='Unlock Your Potential with Ethio Exams Academy - Where Learning Meets Success!'
     keywords='Programming, High School Courses, Freshman Courses, Entrance Exams, Exit Exams, Online Education, Lifelong Learning'
 />
-  <h1>Privacy Policy of Ethio  Exams Academy</h1>
+<MainLayout>
+  <SubLayout className="bg-white dark:bg-gray-800 rounded-[10px]">
+    <div className="p-3">
+    <h1 className="text-xl">Privacy Policy of Ethio  Exams Academy</h1>
 
 <p>Ethio  Exams Academy operates the https://ethio-exams-academy.vercel.app/ website, which provides the SERVICE.</p>
 
@@ -17,21 +22,21 @@ const PrivancyPolicy = () => {
 
 <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at https://ethio-exams-academy.vercel.app/, unless otherwise defined in this Privacy Policy.</p>
 
-<h2>Information Collection and Use</h2>
+<h2 className="text-lg">Information Collection and Use</h2>
 
 <p>For a better experience while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to your name, phone number, and postal address. The information that we collect will be used to contact or identify you.</p>
 
-<h2>Log Data</h2>
+<h2 className="text-lg">Log Data</h2>
 
 <p>We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computer&apos;s Internet Protocol (&quot;IP&quot;) address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.</p>
 
-<h2>Cookies</h2>
+<h2 className="text-lg">Cookies</h2>
 
 <p>Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer&apos;s hard drive.</p>
 
 <p>Our website uses these &quot;cookies&quot; to collection information and to improve our Service. You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our Service.</p>
 
-<h2>Service Providers</h2>
+<h2 className="text-lg">Service Providers</h2>
 
 <p>We may employ third-party companies and individuals due to the following reasons:</p>
 
@@ -44,11 +49,11 @@ const PrivancyPolicy = () => {
 
 <p>We want to inform our Service users that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
 
-<h2>Security</h2>
+<h2 className="text-lg">Security</h2>
 
-<p>We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+<p >We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
 
-<h2>Links to Other Sites</h2>
+<h2 className="text-lg">Links to Other Sites</h2>
 
 <p>Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
 
@@ -56,15 +61,19 @@ const PrivancyPolicy = () => {
 
 <p>Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.</p>
 
-<h2>Changes to This Privacy Policy</h2>
+<h2 className="text-lg">Changes to This Privacy Policy</h2>
 
 <p>We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.</p>
 
 <p>Our Privacy Policy was created with the help of the <a href="https://www.privacypolicytemplate.net">Privacy Policy Template</a>.</p>
 
-<h2>Contact Us</h2>
+<h2 className="text-lg">Contact Us</h2>
 
-<p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p></> );
-}
+<p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p>
+
+    </div>
+  </SubLayout>
+</MainLayout>
+ </> )}
  
 export default PrivancyPolicy;
