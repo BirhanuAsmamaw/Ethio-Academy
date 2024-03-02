a06815e2c00539b2a033563a092c7cc9828f273f
deribewsoftware <deribewsoftware@gmail.com>
2024-03-02 20:05:28 +0300

    updated

--- app/course/[courseId]/reviews.tsx
+++ app/course/[courseId]/reviews.tsx
@@ -26,7 +26,15 @@ const Reviews:React.FC<ReviewsProps> = ({reviews}) => {
       <div className="flex flex-col  justify-between w-full">
         <div className="flex flex-col md:flex-row md:gap-6 justify-between"> 
         <p className="font-bold">{review.customer.name}</p>
-        <p className="text-gray-500 dark:text-gray-400 text-sm"> <Moment fromNow>{review.createdAt}</Moment></p>
+
+
+        <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded me-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500 ">
+<svg className="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z"/>
+</svg>
+<Moment fromNow>{review.createdAt}</Moment>
+</span>
+ 
         </div>
         <div className="">
           <Rating 
