107b4e4e4fb3977790f77634daad29ee38732cc3
deribewsoftware <deribewsoftware@gmail.com>
2024-03-03 18:55:48 +0300

    updated

--- app/dashboard/list-courses/[courseId]/delete/page.tsx
+++ app/dashboard/list-courses/[courseId]/delete/page.tsx
@@ -1,32 +1,39 @@
-"use client"
-
+import { DeleteCourseById } from "@/actions/courses/deleteCoureById";
 import DeleteComponent from "@/components/deleteComponent";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import toast from "react-hot-toast";
 
 interface IParams{
   courseId: string;
 }
-const DeleteCourse = ({params}:{params:IParams}) => {
-  const router=useRouter();
-  const [isLoading,setLoading]=useState(false);
-  const onDelete=()=>{
-    setLoading(true);
- axios.delete(`/api/course/${params.courseId}/delete`).then(()=>{
-      toast.success(`Course deleted successfully`);
-      router.push(`/dashboard/list-courses`);
-      router.refresh();
-    }).catch((error)=>{
-      console.log(error);
-      toast.error(error.message);
-    }).finally(()=>{
-      setLoading(false);
-    });
+const DeleteCourse = async({params}:{params:IParams}) => {
+//   const router=useRouter();
+//   const [isLoading,setLoading]=useState(false);
+//   const onDelete=()=>{
+//     setLoading(true);
+//  axios.delete(`/api/course/${params.courseId}/delete`).then(()=>{
+//       toast.success(`Course deleted successfully`);
+//       router.push(`/dashboard/list-courses`);
+//       router.refresh();
+//     }).catch((error)=>{
+//       console.log(error);
+//       toast.error(error.message);
+//     }).finally(()=>{
+//       setLoading(false);
+//     });
 
-  }
-  return (<DeleteComponent isLoading={isLoading} onDelete={onDelete} title="Delete this Course" label="Delete" />);
+  
+
+const onDelete=async()=>{
+  await DeleteCourseById(params.courseId)
+  console.log(`Course deleted successfully`);
+};
+
+
+
+  return (<DeleteComponent 
+  
+    onDelete={onDelete} 
+    title="Delete this Course"
+    label="Delete" />);
 }
  
 export default DeleteCourse;
