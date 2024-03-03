c37e8d34b20dbce66e4e2e454e4f527f6987d7a3
deribewsoftware <deribewsoftware@gmail.com>
2024-03-03 10:28:41 +0300

    updated

--- actions/courses/getCourses.ts
+++ actions/courses/getCourses.ts
@@ -3,6 +3,9 @@ import prisma from "@/lib/prismadb"
 export async function getCourses(){
   try{
     const courses = await prisma.course.findMany({
+      orderBy:{
+rating:"desc"
+      },
       include:{
         reviews:true,
         chapters:{
