10db2dc63d3502e57eae48ced6103ec94027eedc
deribewsoftware <deribewsoftware@gmail.com>
2024-03-03 14:09:20 +0300

    updated

--- app/dashboard/list-courses/[courseId]/delete/page.tsx
+++ app/dashboard/list-courses/[courseId]/delete/page.tsx
@@ -1,33 +1,29 @@
-"use client"
 
 import { DeleteCourseById } from "@/actions/courses/deleteCoureById";
 import DeleteComponent from "@/components/deleteComponent";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+
 import toast from "react-hot-toast";
 
 interface IParams{
   courseId: string;
 }
-const DeleteCourse = ({params}:{params:IParams}) => {
-  const router=useRouter();
-  const [isLoading,setLoading]=useState(false);
+const DeleteCourse = async({params}:{params:IParams}) => {
+  
   const onDelete = async () => {
-    setLoading(true);
+
     try {
       await DeleteCourseById(params.courseId);
       toast.success(`Course deleted successfully`);
-      router.push(`/dashboard/list-courses`);
+     
     } catch (error) {
       console.error("Delete failed:", error);
       toast.error("Failed to delete course. Please try again.");
     } finally {
-      setLoading(false);
+   
     }
   };
 
-  return (<DeleteComponent isLoading={isLoading} onDelete={onDelete} title="Delete this Course" label="Delete" />);
+  return (<DeleteComponent onDelete={onDelete} title="Delete this Course" label="Delete" />);
 }
  
 export default DeleteCourse;
