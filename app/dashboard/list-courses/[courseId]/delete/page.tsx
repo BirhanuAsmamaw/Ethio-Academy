4683c7a42ed51d48ec08a93528d287d7ee081c9d
deribewsoftware <deribewsoftware@gmail.com>
2024-03-03 14:03:59 +0300

    updated

--- app/dashboard/list-courses/[courseId]/delete/page.tsx
+++ app/dashboard/list-courses/[courseId]/delete/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { DeleteCourseById } from "@/actions/courses/deleteCoureById";
 import DeleteComponent from "@/components/deleteComponent";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -15,7 +16,7 @@ const DeleteCourse = ({params}:{params:IParams}) => {
   const onDelete = async () => {
     setLoading(true);
     try {
-      await axios.delete(`/api/course/${params.courseId}/delete`);
+      await DeleteCourseById(params.courseId);
       toast.success(`Course deleted successfully`);
       router.push(`/dashboard/list-courses`);
     } catch (error) {
