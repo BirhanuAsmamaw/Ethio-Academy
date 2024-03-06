d1253b418022d8e811e3c62802eb08f81c9ebeaf
deribewsoftware <deribewsoftware@gmail.com>
2024-03-06 12:15:37 +0300

    updated

--- app/settings/editPassword.tsx
+++ app/settings/editPassword.tsx
@@ -1,13 +1,15 @@
 
 "use client"
+import Button from "@/components/button/button";
 import Input from "@/components/input/input";
-import { useForm, FieldValues } from "react-hook-form";
-interface EditPasswordProps{
-  user:any;
-}
-const EditPassword:React.FC<EditPasswordProps> = ({user}) => {
+import { useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 
-  const {register,setValue,handleSubmit,getValues,formState:{errors}}=useForm<FieldValues>({
+const EditPassword= () => {
+
+  const [isLoading,setloading] =useState(false);
+
+  const {register,handleSubmit,formState:{errors}}=useForm<FieldValues>({
   
     defaultValues:{
       
@@ -17,12 +19,20 @@ const EditPassword:React.FC<EditPasswordProps> = ({user}) => {
         
     }})
 
+    const onSubmit:SubmitHandler<FieldValues> = (data) => {
+      setloading(true);
+      console.log(data);
+      setloading(false);
+    }
 
   return ( <div className="p-1 flex flex-col gap-4 w-full">
   <h1 className="text-lg font-semibold">Edit Your Password</h1>
   <div className="p-2 space-y-3">
     <Input type="password" label="Old Password" register={register} errors={errors} id="oldPassword"/>
     <Input type="password" label="New password" register={register} errors={errors} id="newPassword"/>
+    <div className="p-6 flex justify-end">
+      <Button isDisabled={isLoading} title={isLoading? "Loading...":"Submit"} onClick={handleSubmit(onSubmit)}/>
+    </div>
   </div>
 </div> );
 }
