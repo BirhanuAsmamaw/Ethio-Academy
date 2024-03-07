02ce5b0c98909e0a6e0b63bd06301d0681d87714
deribewsoftware <deribewsoftware@gmail.com>
2024-03-07 16:03:33 +0300

    updated

--- app/course/[courseId]/[chapterId]/[lessonId]/lessonBlur.tsx
+++ app/course/[courseId]/[chapterId]/[lessonId]/lessonBlur.tsx
@@ -1,13 +1,16 @@
 
 "use client"
 import Container from "@/components/container/container";
+import { useCart } from "@/hooks/use.cart";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-
-const LessonBlur = () => {
+interface LessonBlurProps{
+  course:any;
+}
+const LessonBlur:React.FC<LessonBlurProps> = ({course}) => {
   const router=useRouter();
   const [reloadPage, setReloadPage] = useState(false);
-
+  const {addToCart}=useCart();
   useEffect(() => {
     // Set a timeout to reload the page after 1 minute (60,000 milliseconds)
     const timeoutId = setTimeout(() => {
@@ -18,16 +21,18 @@ const LessonBlur = () => {
     return () => clearTimeout(timeoutId);
   }, []);
 
-  
+  const onPayment=()=>{
+    addToCart(course)
+    router.push('/payment')
+  }
+
+
   return ( <>{reloadPage&&<div className="fixed z-50 flex justify-center items-center h-screen w-full bg-black bg-opacity-80">
     <Container
     childern={
       <div className="px-2 py-6 flex flex-col gap-6 w-72">
         <button
-          onClick={()=>{
-            
-            router.push(`/payment`);
-         }}
+         onClick={onPayment}
           className="text-white bg-green-500 px-4 py-2 rounded-[5px] mr-2 hover:bg-green-600 focus:outline-none"
         >
           Buy Course Now!
