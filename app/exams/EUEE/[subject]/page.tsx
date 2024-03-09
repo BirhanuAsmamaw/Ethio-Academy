4558371371be6081a7a74a5aa805faed94b519d8
deribewsoftware <deribewsoftware@gmail.com>
2024-03-09 16:31:01 +0300

    updated

--- app/exams/EUEE/[subject]/page.tsx
+++ app/exams/EUEE/[subject]/page.tsx
@@ -1,9 +1,31 @@
+import Banner from '@/components/banner'
+import YearExamCard from '@/components/card/yearExamsCard'
+import { examsYears } from '@/lib/examsYear'
 import React from 'react'
 
-const EUEESubject = () => {
+const ExamSubjectPage = ({params}:{params:{subject:string}}) => {
   return (
-    <div>EUEESubject</div>
+    <div className='min-h-screen w-full flex flex-col gap-10 '>
+     <div className="p-4 md:p-6 lg:p-10 xl:p-20">
+     <Banner>
+        <div className="flex  justify-center items-center h-[200px]">
+          <h1 className='text-2xl  text-white font-bold'>{params.subject} Entrance Exams</h1>
+        </div>
+      </Banner>
+     </div>
+
+      <div className="flex justify-center p-4">
+        <div className="w-full gap-4 lg:w-10/12 xl:w-8/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+         {examsYears.map((year,index)=>{
+          return <YearExamCard key={index} year={year} subject={params.subject}/>
+         })}
+         
+          
+        </div>
+      </div>
+
+    </div>
   )
 }
 
-export default EUEESubject
+export default ExamSubjectPage
---
+++ components/card/yearExamsCard.tsx
@@ -0,0 +1,29 @@
+"use client"
+import React from 'react'
+import { useRouter } from 'next/navigation'
+import StartExamDialogue from '../startExamDialogue';
+interface YearExamCardProps{
+  year: string;
+  subject: string;
+}
+const YearExamCard:React.FC<YearExamCardProps> = ({year,subject}) => {
+  const router=useRouter();
+  const onCheckRight=()=>{
+    
+    router.push(`/exams/EUEE/${subject}/${year}?right=${true}`)
+  }
+
+  const onShowAnswer=()=>{router.push(`/exams/EUEE/${subject}/${year}`)}
+  return (
+    <div  className="no-underline block w-full md:max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+     <div className="flex flex-col gap-4 justify-center items-center p-2">
+      <h1 className='text-lg font-semibold'>{year} E.C  Year Exams</h1>
+      <StartExamDialogue 
+      onCheckRightAnswer={onCheckRight} 
+      onShowAnswerAfterQuiz={onShowAnswer}/>
+     </div>
+    </div>
+  )
+}
+
+export default YearExamCard
