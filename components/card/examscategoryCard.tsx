9e59a2f309aa7bb9fb69aef24e22de8458f84da7
deribewsoftware <deribewsoftware@gmail.com>
2024-03-10 16:13:12 +0300

    updated

--- components/card/examscategoryCard.tsx
+++ components/card/examscategoryCard.tsx
@@ -8,7 +8,7 @@ interface ExamsCategoryCardProps{
   image: any;
 }
 const ExamsCategoryCard:React.FC<ExamsCategoryCardProps> = ({name,url,image}) => {
-  return ( <Link href={url}  className="no-underline block w-full md:max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 group">
+  return ( <Link href={url}  className="z-30 no-underline block w-full md:max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 group">
     
   <div className="flex justify-around gap-2">
     <div className="w-full">
