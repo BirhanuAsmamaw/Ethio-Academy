457dd549b03281f5cf4fecd3dc26e3c2e29dcf7c
deribewsoftware <deribewsoftware@gmail.com>
2024-03-11 23:13:39 +0300

    updated

--- actions/departments/getDepartmentById.ts
+++ actions/departments/getDepartmentById.ts
@@ -2,7 +2,10 @@ import prisma from "@/lib/prismadb"
 export async function getDepartmentById(id: string){
   try{
     const department = await prisma.department.findUnique({
-      where:{id: id}
+      where:{id: id},
+      include:{
+        exam:true
+      }
     })
     return department
   }
