ffc554bdc456ca7d10b9656f3273382f07a53629
deribewsoftware <deribewsoftware@gmail.com>
2024-03-11 23:45:24 +0300

    updated

--- app/exams/GAT/[gatDepartmentId]/page.tsx
+++ app/exams/GAT/[gatDepartmentId]/page.tsx
@@ -3,8 +3,8 @@ import GATDepartmentClient from './gatDepartmentClient'
 import { getDepartmentById } from '@/actions/departments/getDepartmentById'
 import Navbar from '@/components/navbar/Navbar'
 
-const GATExamDepartment = async({params}:{params:{GATDepartmentId:string}}) => {
-  const department=await getDepartmentById(params.GATDepartmentId)
+const GATExamDepartment = async({params}:{params:{gatDepartmentId:string}}) => {
+  const department=await getDepartmentById(params.gatDepartmentId)
   return (<>
   <Navbar/>
   <GATDepartmentClient department={department}/>
