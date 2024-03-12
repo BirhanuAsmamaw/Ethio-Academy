38cf1dd82761936642375f03b52b2d5536a0c511
deribewsoftware <deribewsoftware@gmail.com>
2024-03-12 09:03:35 +0300

    updated

--- components/banner.tsx
+++ components/banner.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 const Banner = ({children}:{children:ReactNode}) => {
   return (
-    <div className='w-full rounded-[10px] p-2 bg-green-500'>
+    <div className='w-full rounded-[10px] p-2 light-banner  dark:dark-banner '>
       {children}
     </div>
   )
