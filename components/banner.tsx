120d2eb9f3afe081aed5139fd53aab5ee12b8d5e
deribewsoftware <deribewsoftware@gmail.com>
2024-03-13 09:46:37 +0300

    updated

--- components/banner.tsx
+++ components/banner.tsx
@@ -7,7 +7,7 @@ interface Bannerprops{
 const Banner:React.FC<Bannerprops> = ({title,children}) => {
   return (
     <div className="w-full rounded-[10px]  p-2 bg-[url('../public/lightBanner.png')] dark:bg-[url('../public/darkBanner.png')] ">
-      <div className='space-y-10'>
+      <div className='space-y-4'>
     
     <div className="flex  justify-center items-center h-[200px] lg:h-[400px]">
       <h1 className=' text-xl md:text-2xl lg:text-4xl xl:text-6xl text-blue-700 
