e288208c89f9d1fac12340eac6edd83d44d776fc
deribewsoftware <deribewsoftware@gmail.com>
2024-03-13 10:53:59 +0300

    updated

--- app/exams/EUEE/page.tsx
+++ app/exams/EUEE/page.tsx
@@ -4,6 +4,7 @@ import ExamsCategoryCard from '@/components/card/examscategoryCard'
 import Navbar from '@/components/navbar/Navbar'
 import { eueeSubjects } from '@/lib/eueeSubjects'
 import React from 'react'
+import BuyExamButton from '../buyExamButton'
 
 const EUEEPage = () => {
   return (
@@ -13,18 +14,7 @@ const EUEEPage = () => {
      <div className="p-4 md:p-6 lg:p-10 xl:p-20">
      <Banner title="Ethiopian University Entrance Exams">
         
-     <div className='flex justify-center w-full gap-6'>
-    <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-bold text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-Buy Exams Now!
-</span>
-</button>
-<button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-bold text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-See Tray Exams
-</span>
-</button>
-    </div>
+     <BuyExamButton/>
      </Banner>
      </div>
 
