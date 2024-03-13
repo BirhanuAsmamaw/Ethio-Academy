bd4ad8966ea52025a3daa7efd420f1206db6a306
deribewsoftware <deribewsoftware@gmail.com>
2024-03-13 11:21:31 +0300

    updated

--- app/exams/SPHMMEC/page.tsx
+++ app/exams/SPHMMEC/page.tsx
@@ -4,6 +4,7 @@ import YearExamCard from '@/components/card/yearExamsCard'
 import Navbar from '@/components/navbar/Navbar'
 import { examsYears } from '@/lib/examsYear'
 import React from 'react'
+import BuyExamButton from '../buyExamButton'
 
 const SPHMMECPage = () => {
   return (
@@ -12,19 +13,7 @@ const SPHMMECPage = () => {
     <div className='min-h-screen w-full flex flex-col gap-10  pt-10'>
      <div className="p-4 md:p-6 lg:p-10 xl:p-20">
      <Banner title="St.Paul's Hospital Millennium Medicine Entrance COC Exams<">
-        
-        <div className='flex justify-center w-full gap-6'>
-    <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-bold text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-Buy Exams Now!
-</span>
-</button>
-<button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-bold text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-See Tray Exams
-</span>
-</button>
-    </div>
+      <BuyExamButton/>
       </Banner>
      </div>
 
