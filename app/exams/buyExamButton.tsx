55e63fa77f181fceacbaaca2777a3722834b3ef0
deribewsoftware <deribewsoftware@gmail.com>
2024-03-13 11:10:09 +0300

    updated

--- app/exams/buyExamButton.tsx
+++ app/exams/buyExamButton.tsx
@@ -1,12 +1,12 @@
 const BuyExamButton = () => {
   return ( <div className='flex flex-col md:flex-row justify-center w-full gap-6'>
-  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden rounded-full font-bold text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden rounded-full font-bold text-gray-900  group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+<span className="relative px-5 py-2.5  w-full transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Buy Exams Now!
 </span>
 </button>
-<button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden rounded-full font-bold text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+<button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden rounded-full font-bold text-gray-900  group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+<span className="relative px-5 py-2.5 w-full  transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 See Tray Exams
 </span>
 </button>
