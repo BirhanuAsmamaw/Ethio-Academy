52e5e348cf3915d67c05fb5c2550c7866c5cc002
deribewsoftware <deribewsoftware@gmail.com>
2024-03-20 19:25:35 +0300

    updated

--- components/input/regInput.tsx
+++ components/input/regInput.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 
 const RegInput = () => {
-  return (<div className="relative mb-3" >
-  <input type="password"
-    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-    id="exampleFormControlInputPassword"
-    placeholder="Example label" />
-  <label
-    htmlFor="exampleFormControlInputPassword"
-    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
-    >Password input
+  return (<div className="relative w-full min-w-[200px] h-10">
+  <input
+    className="peer
+     w-full h-full 
+     bg-transparent
+      text-blue-gray-700
+       font-sans font-normal
+        outline 
+        outline-0 
+        focus:outline-0 
+        disabled:bg-blue-gray-50 
+        disabled:border-0 
+        transition-all 
+        placeholder-shown:border
+         placeholder-shown:border-blue-gray-200 
+        placeholder-shown:border-t-blue-gray-200
+         border 
+         darK:border-gray-600
+         focus:border-2 border-t-transparent
+          focus:border-t-transparent text-sm 
+          px-3 py-2.5 rounded-[7px] 
+          border-blue-gray-200 focus:border-blue-600"
+    placeholder=" " /><label
+    className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">Username
   </label>
 </div>
   )
