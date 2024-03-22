358ce5f887d44bee01f37125cf4f586a8f64386e
deribewsoftware <deribewsoftware@gmail.com>
2024-03-22 13:01:48 +0300

    updated approved courses  list table

--- actions/courses/getCourseById.ts
+++ actions/courses/getCourseById.ts
@@ -3,20 +3,34 @@ import prisma from '@/lib/prismadb'
 export async function GetCourseById(courseId: string){
   try{
     const course = await prisma.course.findUnique({
-      where:{id: courseId},
-     
-      include:{
-        reviews:true,
-        chapters:{
-          include: {
-            lessons:true
+      where:{
+       
+        id:courseId,
+      },
+      include:{chapters:{
+        include:{
+          lessons:{
+            include:{
+              questions:true
+            }
           }
         }
-      }
+      },
+        reviews:{
+          orderBy:{
+            createdAt:"desc"
+          },
+          include:{
+            customer:true
+          }
+      }},
       
     });
-  return course
-  }catch(e){
+    return course
+    
+  }
+  
+  catch(e){
     return null;
   }
 }
