ac5b05a438aaed8a6044253dfaa86afe2dfc770b
deribewsoftware <deribewsoftware@gmail.com>
2024-03-23 15:23:58 +0300

    updated

--- app/dashboard/list-courses/[courseId]/chapter/chapterList.tsx
+++ app/dashboard/list-courses/[courseId]/chapter/chapterList.tsx
@@ -70,7 +70,7 @@ export function ChapterList({course}:{course:CourseType|any}) {
    hover:bg-gray-100 hover:text-blue-700 focus:z-10 
    focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700
     dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600
-     dark:hover:text-white dark:hover:bg-gray-700 flex gap-2 items-center justify-center" value="delete"><><MdDelete size={24}/> <p>DelteChapter</p></></TabsTrigger>
+     dark:hover:text-white dark:hover:bg-gray-700 flex gap-2 items-center justify-center" value="delete"><><MdDelete size={24}/> <p>Delete Chapter</p></></TabsTrigger>
   </TabsList>
 
   <TabsContent value="update" className="pt-10">
