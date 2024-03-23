3b353ca1cb46c854e9f4862d8e51a02950c54920
deribewsoftware <deribewsoftware@gmail.com>
2024-03-23 13:43:36 +0300

    updated

--- app/dashboard/list-courses/[courseId]/chapter/page.tsx
+++ app/dashboard/list-courses/[courseId]/chapter/page.tsx
@@ -27,7 +27,7 @@ const Chapter = async({params}:{params:IParams}) => {
 
 
   return ( <div className="flex  min-h-screen items-center justify-center w-full  p-4 lg:p-10">
-  <div className="w-full lg:px-10  xl:px-20 2xl:px-32 bg-white dark:bg-slate-800 p-2 flex flex-col gap-2">
+  <div className="w-full  xl:w-8/12 bg-white dark:bg-slate-800 p-2 flex flex-col gap-2">
     
   <CreateChapter courseId={params.courseId}/>
     <ChapterList course={course}/>
