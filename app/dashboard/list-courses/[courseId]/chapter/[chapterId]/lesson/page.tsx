2f894b0e0c0cbc71f3c1cb9e0ff791baa88fce9d
deribewsoftware <deribewsoftware@gmail.com>
2024-03-23 09:32:34 +0300

    updated

--- app/dashboard/list-courses/[courseId]/chapter/[chapterId]/lesson/page.tsx
+++ app/dashboard/list-courses/[courseId]/chapter/[chapterId]/lesson/page.tsx
@@ -116,7 +116,7 @@ const onBack=()=>{
 focus:ring-4 focus:outline-none focus:ring-blue-300 
 disabled:bg-blue-400 disabled:dark:bg-blue-500 disabled:cursor-not-allowed
  font-medium rounded-lg text-sm px-5 py-2.5 text-center" 
-disabled={isDisabled}
+
 >Next</button>
 
 </div>
