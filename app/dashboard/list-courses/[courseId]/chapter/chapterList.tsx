cc91255ffc1f18a0e3ed9818f86e1bd4776bd98a
deribewsoftware <deribewsoftware@gmail.com>
2024-03-23 14:41:08 +0300

    updated

--- app/dashboard/list-courses/[courseId]/chapter/chapterList.tsx
+++ app/dashboard/list-courses/[courseId]/chapter/chapterList.tsx
@@ -56,7 +56,7 @@ export function ChapterList({course}:{course:CourseType|any}) {
           <div className="p-4 flex flex-col md:flex-row md:justify-between">
            
 
-            <Tabs defaultValue="" className=" shadow-lg dark:shadow-black border dark:border-gray-700 rounded-[10px] min-w-[350px] max-w-[600px] bg-white dark:bg-gray-800">
+            <Tabs defaultValue="" className=" w-full">
   <TabsList className="grid w-full grid-cols-1 md:grid-cols-2 gap-4 ">
     <TabsTrigger className="py-2 md:py-2.5 px-3 md:px-5 me-2 mb-2
   text-sm font-medium text-gray-900 focus:outline-none
@@ -65,7 +65,13 @@ export function ChapterList({course}:{course:CourseType|any}) {
    focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700
     dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600
      dark:hover:text-white dark:hover:bg-gray-700 flex gap-2 items-center justify-center" value="update"><><MdModeEdit size={24}/> <p>Update Chapter</p></></TabsTrigger>
-    <TabsTrigger className="rounded-full"  value="delete">delete</TabsTrigger>
+    <TabsTrigger className="py-2 md:py-2.5 px-3 md:px-5 me-2 mb-2
+  text-sm font-medium text-gray-900 focus:outline-none
+   bg-white rounded-full border border-gray-200 
+   hover:bg-gray-100 hover:text-blue-700 focus:z-10 
+   focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700
+    dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600
+     dark:hover:text-white dark:hover:bg-gray-700 flex gap-2 items-center justify-center" value="delete"><><MdDelete size={24}/> <p>DelteChapter</p></></TabsTrigger>
   </TabsList>
 
   <TabsContent value="update">
