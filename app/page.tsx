f803fdf028544128325cf987a6cf4016831f5589
deribewsoftware <deribewsoftware@gmail.com>
2024-03-23 23:40:50 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -46,11 +46,13 @@ const examsCategory=await getAllExamsCategory();
     <Hero/>
    
    
+
+   {/* Courses */}
     <div id="courseslist" className=" min-h-screen flex flex-col gap-10 overflow-hidden">
 
 
     <div className="flex justify-center w-full">
-    <div className="w-full lg:w-11/12 space-y-4">
+    <div className="w-full lg:w-11/12 xl:px-20 space-y-4">
       <h1 className='w-full text-xl md:text-4xl font-semibold border-b-2 border-double  p-2 dark:text-gray-300 border-gray-200 dark:border-gray-700 pl-4'>Most common Courses</h1>
   
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-4 p-4 md:px-10'>
@@ -78,7 +80,7 @@ const examsCategory=await getAllExamsCategory();
 
 
    <div className="flex justify-center w-full ">
-    <div className="w-full lg:w-11/12 space-y-4">
+    <div className="w-full lg:w-11/12 space-y-4 xl:px-20">
     <h1 className='w-full text-xl md:text-4xl font-semibold border-b-2 border-double p-2 dark:text-gray-300 border-gray-200 dark:border-gray-700 pl-4'>New Coming  Courses</h1>
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-4 p-4 md:px-10'>
 
