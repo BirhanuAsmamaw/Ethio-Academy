cbd014232586b762726a860b12d1a8c98340b7e1
deribewsoftware <deribewsoftware@gmail.com>
2024-03-23 13:30:26 +0300

    updated

--- app/dashboard/list-courses/[courseId]/chapter/page.tsx
+++ app/dashboard/list-courses/[courseId]/chapter/page.tsx
@@ -27,25 +27,12 @@ const Chapter = async({params}:{params:IParams}) => {
 
 
   return ( <div className="flex  min-h-screen items-center justify-center w-full  p-4 lg:p-10">
-  <div className="w-full lg:w-8/12 xl:w-7/12 2xl:w-1/2 bg-white p-2 flex flex-col gap-2">
+  <div className="w-full lg:w-8/12 xl:w-7/12 2xl:w-1/2 bg-white dark:bg-gray-800 p-2 flex flex-col gap-2">
     
   <CreateChapter courseId={params.courseId}/>
     <ChapterList course={course}/>
   </div>
 
-<div className="w-full flex justify-center py-4">
-        <h1 className="text-xl dark:text-white font-bold">{course?.subject}</h1>
-      </div>
-
-
-
-
-
-
-   {/* chapter lists */}
-   <div className="mt-10">
-  <ChapterList course={course}/>
-   </div>
   </div> );
 }
  
