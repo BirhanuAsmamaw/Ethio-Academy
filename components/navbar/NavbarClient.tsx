01e751ad7d04e89dd9d697480dde01f9b58de8cb
deribewsoftware <deribewsoftware@gmail.com>
2024-03-24 09:35:00 +0300

    updated

--- components/navbar/NavbarClient.tsx
+++ components/navbar/NavbarClient.tsx
@@ -48,7 +48,7 @@ const {carts}=useCart()
 
   return ( <div className={` w-full px-4 md:px-6 z-50 h-14 items-center   flex justify-between flex-wrap  ${isScroll? "shadow-md shadow-slate-300 dark:shadow-black fixed bg-white dark:bg-gray-900  ":"bg-stone-200 dark:bg-gray-900 "} duration-300 z-50`}>
     <Link href="/" className="no-underline flex gap-2">
-      <Avatar className={`${!user?.image&&'hidden'} h-10 w-10`}>
+      <Avatar className={` h-10 w-10`}>
       <AvatarImage src="../../public/logo.png" alt="EEA" />
       <AvatarFallback>EEA</AvatarFallback>
     </Avatar>
