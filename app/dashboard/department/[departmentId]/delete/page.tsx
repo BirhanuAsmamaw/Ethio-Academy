6f6789aef8a6c7d9e07bbe6cc0993db42a0f434f
deribewsoftware <deribewsoftware@gmail.com>
2024-03-25 23:39:18 +0300

    Updated

--- app/dashboard/department/[departmentId]/delete/page.tsx
+++ app/dashboard/department/[departmentId]/delete/page.tsx
@@ -2,9 +2,8 @@ import DeleteComponent from '@/components/deleteComponent'
 import React from 'react'
 
 const DeleteDepartment = () => {
-  return (<DeleteComponent onDelete={function (): void {
-    throw new Error('Function not implemented.')
-  } } title={' Delete this Department'} label={'Delete'}/>
+  const onDelete = () => {};
+  return (<DeleteComponent onDelete={onDelete} title={' Delete this Department'} label={'Delete'}/>
   )
 }
 
--- app/dashboard/exams/type/[examTypeId]/delete/page.tsx
+++ app/dashboard/exams/type/[examTypeId]/delete/page.tsx
@@ -2,9 +2,8 @@ import DeleteComponent from '@/components/deleteComponent'
 import React from 'react'
 
 const ExamTypeDelete = () => {
-  return (<DeleteComponent onDelete={function (): void {
-    throw new Error('Function not implemented.')
-  } } title={'Delete This Exam type'} label={'Delete'}/>
+  const onDelete = () => {};
+  return (<DeleteComponent onDelete={onDelete} title={'Delete This Exam type'} label={'Delete'}/>
   )
 }
 
