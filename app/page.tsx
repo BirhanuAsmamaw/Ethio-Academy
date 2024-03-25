b3b9be6795af999112b4d90bcb6b46d26466b48e
deribewsoftware <deribewsoftware@gmail.com>
2024-03-25 23:25:15 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -108,14 +108,14 @@ const examsCategory=await getAllExamsCategory();
     {/* 
 EXAMS CATEGORY */}
   <div className="flex  justify-center w-full pt-10">
-    <div className="w-full lg:w-11/12   space-y-4">
+    <div className="w-full lg:w-11/12 xl:px-20   space-y-4">
 
 <h1 className='w-full text-xl md:text-4xl font-semibold border-b-2 border-double p-2 dark:text-gray-300 border-gray-200 dark:border-gray-700 pl-4'>Exams</h1>
 
 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-4 p-4 md:px-10'>
        {examsCategory?.map((exam)=>{
         return <ExamsCategoryCard key={exam.id} name={exam.examType} 
-        url={`/exams/${exam.examType==="AAU  Medicine Entrance COC"? `AAUMEC`:`${exam.examType==="St.Paul's Hospital Millennium Medicine Entrance COC"? `SPHMMEC`:`${exam.examType}`}`}`} 
+        url={`/exams/${exam.url} `}
         image={exam.cover?.public_url}/>
        })}
 
