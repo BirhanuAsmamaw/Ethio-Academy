3f70166f6594c328fdb8248959e2f6fcd80f8667
deribewsoftware <deribewsoftware@gmail.com>
2024-03-27 01:45:46 +0300

    Updated

---
+++ app/dashboard/user-list/[userId]/delete/deleteuserPage.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import DeleteComponent from '@/components/deleteComponent';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+
+interface DeleteUserClientPageProps{
+  user:any
+}
+const DeleteUserClientPage:React.FC<DeleteUserClientPageProps>= ({user}) => {
+  const [isLoading,setLoading]=useState(false)
+  const router=useRouter();
+    const onSubmit=()=>{
+      setLoading(true);
+      axios.delete(`/api/user/${user.id}/delete`).then(async()=>{
+        
+        // if(user.image){
+        //   await RemoveFile(user.cover.public_key);
+        // }
+        
+        toast.success("user deleted successfully")
+        router.back();
+        router.refresh()
+        
+      }).catch((errors)=>{
+        toast.error("something went wrong");
+      }).finally(()=>{
+        setLoading(false);
+      });
+     
+    
+    }
+    if(!user){
+      router.back();
+      return null;
+    }
+  return (<DeleteComponent isLoading={isLoading}  onDelete={onSubmit} title={user.name} />
+  )
+}
+
+export default DeleteUserClientPage
--- app/dashboard/user-list/[userId]/delete/page.tsx
+++ app/dashboard/user-list/[userId]/delete/page.tsx
@@ -1,7 +1,9 @@
-const DeleteUser=()=>{
-  return (<div>
-    <h1>Delete User</h1>
-  </div>)
+import { getUserById } from "@/actions/tokens/getUserbyd";
+import DeleteUserClientPage from "./deleteuserPage";
+
+const DeleteUser=async({params}:{params:{userId:string}})=>{
+  const user = await getUserById(params.userId);
+  return (<DeleteUserClientPage user={user}/>)
 }
 
 export default DeleteUser;
