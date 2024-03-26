4a8d103b17d52321ca9bd75ad2e1143eeec33d8a
deribewsoftware <deribewsoftware@gmail.com>
2024-03-26 21:08:18 +0300

    Updated

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -62,14 +62,14 @@ const Hero = () => {
     {/* content */}
     <div className="pt-10 md:pt-0 flex flex-col gap-20 justify-center items-center  md:w-2/3">
       <div className="flex flex-col gap-6">
-      <motion.h1 variants={textVariants} className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold items-center justify-center text-center overflow-hidden">Ethio Exams Academy, where heroes are born and excellence reigns supreme <span className="font-bold text-rose-500 dark:text-yellow-400 text-3xl md:text-5xl">!</span></motion.h1>
+      <motion.h1 variants={textVariants} className=" drop-shadow-md text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold items-center justify-center text-center overflow-hidden">Ethio Exams Academy, where heroes are born and excellence reigns supreme <span className="font-bold text-rose-500 dark:text-yellow-400 text-3xl md:text-5xl">!</span></motion.h1>
       <div className="font-medium md:text-lg flex flex-col gap-1 md:mx-10 ">
       <p className="text-md px-4 font-medium dark:text-gray-400 text-slate-500 flex flex-col "> 
-      <motion.span variants={textVariants}  className="text-rose-700 dark:text-yellow-300">🌟 Ignite Your Brilliance:</motion.span> 
-      <motion.span variants={textVariants} className="pl-6 md:pl-2">Join a community of trailblazers where every individual is a beacon of inspiration.</motion.span></p>
+      <motion.span variants={textVariants}  className="text-rose-700 dark:text-yellow-300 drop-shadow-md">🌟 Ignite Your Brilliance:</motion.span> 
+      <motion.span variants={textVariants} className="pl-6 md:pl-2 drop-shadow-md">Join a community of trailblazers where every individual is a beacon of inspiration.</motion.span></p>
       <p className="text-md px-4 font-medium dark:text-gray-400  text-slate-500 flex flex-col "> 
-      <motion.span variants={textVariants} className="text-rose-700 dark:text-yellow-300">💡 Ethio Exams  Academy:</motion.span> 
-      <motion.span  variants={textVariants} className="pl-6 md:pl-2">Is not just a school; its a launchpad for greatness.</motion.span></p>
+      <motion.span variants={textVariants} className="text-rose-700 dark:text-yellow-300 drop-shadow-md">💡 Ethio Exams  Academy:</motion.span> 
+      <motion.span  variants={textVariants} className="pl-6 md:pl-2 drop-shadow-md">Is not just a school; its a launchpad for greatness.</motion.span></p>
       </div>
       </div>
     
@@ -82,11 +82,11 @@ const Hero = () => {
   {users.map((user,index) =><div key={index} className="relative flex shrink-0 select-none items-center justify-center bg-slate-200 font-bold  dark:bg-gray-800 uppercase text-slate-800 dark:text-gray-400 h-14 w-14 rounded-full ring ring-white dark:ring-gray-600">
     <Image height={50} width={50} src={user} className="rounded-full h-full w-full object-cover object-center" alt="Avatar" loading="lazy"/>
   </div>)}
-  <div className="relative flex shrink-0 select-none items-center justify-center bg-slate-200 font-bold  dark:bg-gray-800 uppercase text-slate-800 dark:text-gray-400 h-14 w-14 rounded-full ring ring-white dark:ring-gray-600">
-<span className="text-lg font-bold">10k+</span>
+  <div className="relative flex shrink-0  drop-shadow-md select-none items-center justify-center bg-slate-200 font-bold  dark:bg-gray-800 uppercase text-slate-800 dark:text-gray-400 h-14 w-14 rounded-full ring ring-white dark:ring-gray-600">
+<span className="text-lg font-bold drop-shadow-md">10k+</span>
   </div>
   </motion.div>
-  <motion.h1 variants={textVariants} className="px-2 py-3 text-xl font-semibold  text-blue-700 dark:text-green-400 "> Students Trust us.</motion.h1></div>
+  <motion.h1 variants={textVariants} className="px-2 py-3 text-xl font-semibold  text-blue-700 dark:text-green-400 drop-shadow-md "> Students Trust us.</motion.h1></div>
     </div>
 
     
