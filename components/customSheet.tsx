2a1cf3028e24ac9fbf2b04403230d994ed90600a
deribewsoftware <deribewsoftware@gmail.com>
2024-03-28 17:42:14 +0300

    updated

--- components/customSheet.tsx
+++ components/customSheet.tsx
@@ -25,10 +25,10 @@ const CustomeSheet:React.FC<CustomeSheetProps>= ({children,selectedLabel,unselec
 
 
 <div className="">
-  <button onClick={onOpen} className="pt-2 no-underline text-gray-500 dark:text-gray-400 font-medium  hover:dark:text-green-400 hover:text-rose-400 transition duration-300 font-medium"> 
+  <button onClick={onOpen} className="pt-2 no-underline text-gray-500 dark:text-gray-400  hover:dark:text-green-400 hover:text-rose-400 transition duration-300 font-medium"> 
   {isOpen? <>{selectedLabel}</>:<>{unselectedLabel}</>}
   </button>
-  <div className={`fixed  right-0  h-full bg-white dark:bg-gray-800 shadow-xl top-10 z-50 ${!isOpen?'bg-opacity-0 translate-x-[100%]':'w-[70%] bg-opacity-100 translate-x-0'} transition duration-300 ease-in-out`}>
+  <div className={`fixed  right-0  h-full bg-white dark:bg-gray-800 shadow-xl top-10 z-50 ${!isOpen?'bg-opacity-0 translate-x-[100%]':'w-[80%] bg-opacity-100 translate-x-0'} transition duration-300 ease-in-out`}>
       <div className="  pt-10 w-full space-y-4 overflow-y-auto">
       
       {children}
