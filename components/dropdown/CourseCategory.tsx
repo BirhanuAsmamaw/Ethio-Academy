8ff76350852402ab0c71b9a8d89fd9c51b04674e
deribewsoftware <deribewsoftware@gmail.com>
2024-03-28 22:05:07 +0300

    updated

--- components/dropdown/CourseCategory.tsx
+++ components/dropdown/CourseCategory.tsx
@@ -12,7 +12,7 @@ const ExamsCDrobDown:React.FC<ExamsCDrobDownProps> = ({exams}) => {
       <p>Exams</p>
     } body={<div>
         {exams?.map((c,index)=>{
-            return <Link key={index} href={`/exams/${c.url}`} className="px-2 py-1 text-sm flex no-underline  gap-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+            return <Link key={index} href={`/exams/${c.url}`} className="px-2 divide-y divide-slate-300 dark:divide-gray-600 py-1 text-sm flex no-underline  gap-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
             {c.examType}
            </Link>
           })}
