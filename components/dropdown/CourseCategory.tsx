4ae6ae88ac3e895c1584272490ee3ed05432650f
deribewsoftware <deribewsoftware@gmail.com>
2024-03-28 17:01:32 +0300

    updated

--- components/dropdown/CourseCategory.tsx
+++ components/dropdown/CourseCategory.tsx
@@ -1,34 +1,17 @@
 import Link from "next/link";
 import CDropDown from "./CustomeDropdown/CDropDown";
+import { getAllExamsCategory } from "@/actions/examsCategory/getAllExamsCategry";
 
-const CategorCDrobDown = () => {
-  const category=[
-    {
-      name: "EUEE",
-      url: "/category/Highschool"
-    },
-    {
-      name: "Exit Exams",
-      url: "/category/Freshman"
-    },
-    {
-      name: "Remedial Exams",
-      url: "/category/Remedial"
-    },
-    {
-      name: "GAT Exams",
-      url: "/category/Development"
-    }
-    
-    
-  ]
+const CategorCDrobDown = async() => {
+  const exams=await getAllExamsCategory();
+ 
   return ( 
     <CDropDown chevron title={
-      <p>Category</p>
+      <p>Exams</p>
     } body={<div>
-        {category.map((c,index)=>{
-            return <Link key={index} href={c.url} className="px-2 py-1 text-sm flex no-underline  gap-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-            {c.name}
+        {exams?.map((c,index)=>{
+            return <Link key={index} href={`/exams/${c.url}`} className="px-2 py-1 text-sm flex no-underline  gap-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+            {c.examType}
            </Link>
           })}
     </div>}/>
