809cb68c4cde99a0d5bb123b58bf255f9d5a5566
deribewsoftware <deribewsoftware@gmail.com>
2024-03-29 21:49:54 +0300

    updated

--- app/dashboard/bank/create/page.tsx
+++ app/dashboard/bank/create/page.tsx
@@ -31,10 +31,10 @@ const CreateBank = () => {
     });
 
   }
-  return (<div className="min-h-screen w-full flex justify-center items-center">
-<div className='space-y-6'>
+  return (<div className="min-h-screen w-full flex justify-center items-center p-2">
+<div className='space-y-6  bg-white dark:bg-gray-800 w-full md:w-[400px] p-2 rounde-[10px]'>
   <h1 className='text-lg font-semibold'>Create Bank</h1>
-  <div className="grid grid-cols-1 md:grid-col-2 xl:grid-col-3 gap-4 bg-white dark:bg-gray-800">
+  <div className="grid grid-cols-1 md:grid-col-2 xl:grid-col-3 gap-4">
     <Input id='bank_name' label='Bank Name' register={register} errors={errors} type={'text'} required placehoder='write name of bank'/>
     <Input id='name' label='Your Full Name' register={register} errors={errors} type={'text'} required placehoder='write your full name '/>
     <Input id='accpuount' label='Bank Name' register={register} errors={errors} type={'text'} required placehoder='write bank account number'/>
