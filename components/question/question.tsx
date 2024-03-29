6197ab79fe65dc71efa0d38747bd110c9cc267e9
deribewsoftware <deribewsoftware@gmail.com>
2024-03-29 13:22:32 +0300

    updated

--- components/question/question.tsx
+++ components/question/question.tsx
@@ -19,11 +19,10 @@ interface QuestionComponentProps{
     notificationUrl:string,
     notificationLabel:string,
     examsTitle:string,
-    backUrl?:string,
     Questions:any[];
 
 }
-const QuestionComponent:React.FC<QuestionComponentProps> = ({backUrl,Questions,notificationTitle,notificationUrl,examsTitle,notificationLabel}) => {
+const QuestionComponent:React.FC<QuestionComponentProps> = ({Questions,notificationTitle,notificationUrl,examsTitle,notificationLabel}) => {
   const [feedbackVisible, setFeedbackVisible] = useState(false);
   const [isSelectedAll, setSelectedAll] = useState(false);
   const [score, setScore] = useState(0);
@@ -81,12 +80,12 @@ const QuestionComponent:React.FC<QuestionComponentProps> = ({backUrl,Questions,n
           <div className="p-2 space-y-2 py-6">
             <div className="flex justify-between px-10">
             <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+<span className="relative  px-2 md:px-5 py-1 md:py-2.5  transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Previuos Year Exam
 </span>
 </button>
 <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+<span className="relative px-2 md:px-5 py-1 md:py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Next Year Exam
 </span>
 </button>
