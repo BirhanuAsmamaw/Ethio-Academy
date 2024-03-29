568a54a7519d1245c7b14721e8673579e05b8589
deribewsoftware <deribewsoftware@gmail.com>
2024-03-29 13:38:39 +0300

    updated

--- components/question/question.tsx
+++ components/question/question.tsx
@@ -82,7 +82,7 @@ const QuestionComponent:React.FC<QuestionComponentProps> = ({Questions,notificat
          <p> Back to Exams List</p>
           </button>
           <div className="p-2 space-y-2 py-6">
-            <div className="flex justify-between md:px-10">
+            <div className="flex justify-between md:px-10 pb-6">
             <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
 <span className="relative  px-2 md:px-5 py-1 md:py-2.5  transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Previuos Year Exam
@@ -94,7 +94,7 @@ Next Year Exam
 </span>
 </button>
             </div>
-            <h4 className="text-lg mt-6 font-semibold border-b-2 border-dashed">
+            <h4 className="text-lg  font-semibold border-b-2 border-dashed">
            {examsTitle}
             </h4>
           </div>
