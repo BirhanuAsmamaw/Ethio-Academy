893f93949742514d1c358df0968f414c93652a0f
deribewsoftware <deribewsoftware@gmail.com>
2024-04-02 13:36:17 +0300

    updated

--- app/dashboard/exams/create-exams/createExamsClientPage.tsx
+++ app/dashboard/exams/create-exams/createExamsClientPage.tsx
@@ -387,7 +387,7 @@ onClick={onSubmit}
 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-4 p-4 md:px-10'>
        {exams?.map((exam)=>{
         return <ExamsCategoryCard key={exam.id} name={exam.examType} 
-        url={`dashboard/exams/create-exams/${exam.id} `}
+        url={`/dashboard/exams/create-exams/${exam.id} `}
         image={exam.cover?.public_url}/>
        })}
 
