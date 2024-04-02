978aadf33fc55ef1d2ade2ef5f7ed361f0049210
deribewsoftware <deribewsoftware@gmail.com>
2024-04-02 20:05:33 +0300

    updated

--- app/dashboard/exams/create-exams/[examTypeId]/[departmentId]/question/page.tsx
+++ app/dashboard/exams/create-exams/[examTypeId]/[departmentId]/question/page.tsx
@@ -3,6 +3,7 @@ import Spinning from '@/components/spinning'
 import React, { Suspense } from 'react'
 import QuestionsClient from './questionClient'
 
+
 const QuestionPage = async({params}:{params:{examTypeId:string;departmentId:string}}) => {
   return (<Suspense fallback={<div 
     className="h-screen w-full flex justify-center items-center  gap-1 text-xl">
