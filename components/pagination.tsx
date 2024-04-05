d12a4ca99bb8b2412a07634c2352e5dbbbaf42da
deribewsoftware <deribewsoftware@gmail.com>
2024-04-05 20:54:30 +0300

    context on home page  added

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -14,7 +14,7 @@ const PaginationComponent = () => {
     for (let i = 1; i <= (40/4); i++) {
       items.push(
         <PaginationItem key={i} className="list-none">
-          <PaginationLink className="no-underline" href={`/#courseslist?page=${i}`}>
+          <PaginationLink className="no-underline" href={`/?page=${i}#courseslist`}>
             {i}
           </PaginationLink>
         </PaginationItem>
