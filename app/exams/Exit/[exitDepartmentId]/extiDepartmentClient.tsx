a9a4ea6c9b1b2755228046800b6028201a5ac0ac
deribewsoftware <deribewsoftware@gmail.com>
2024-04-06 14:56:10 +0300

    univerity search added

--- app/exams/Exit/[exitDepartmentId]/extiDepartmentClient.tsx
+++ app/exams/Exit/[exitDepartmentId]/extiDepartmentClient.tsx
@@ -48,7 +48,7 @@ const ExitDepartmentClient:React.FC<ExitDepartmentClientProps> = ({department,is
   <div className="flex justify-center p-4 py-20">
         <div className="w-full gap-4 lg:w-10/12 xl:w-8/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {exitExamYears.map((year,index)=>{
-          return <YearExamCard key={index} year={year} url={`/Exit/${department.id}/${year}/model?university=AAU`}/>
+          return <YearExamCard key={index} year={year} url={`/Exit/${department.id}/${year}/model?university=AAU&&`}/>
          })}
          
          
