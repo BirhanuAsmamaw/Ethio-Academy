ea433ef6976610a0a7f4527bc969f5fcfab2d709
deribewsoftware <deribewsoftware@gmail.com>
2024-04-06 23:10:52 +0300

    updated

--- components/card/yearExamsCard.tsx
+++ components/card/yearExamsCard.tsx
@@ -15,7 +15,7 @@ const YearExamCard:React.FC<YearExamCardProps> = ({year,url}) => {
 
   const onShowAnswer=()=>{router.push(`/exams/${url}`)}
   return (
-    <div  className="no-underline block w-full md:max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div  className="no-underline block w-full md:max-w-sm p-6 h-[100px] bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
      <div className="flex flex-col gap-4 justify-center items-center p-2">
       <h1 className='text-lg font-semibold'>{year} E.C  Year Exams</h1>
       <StartExamDialogue 
