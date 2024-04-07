5f07ad74258fee664e06bc1fa17b8e310caeabdd
deribewsoftware <deribewsoftware@gmail.com>
2024-04-07 10:06:06 +0300

    coc exmas questios list,update,delete added

--- app/dashboard/exam-questions/COC/[subject]/[year]/add-question/page.tsx
+++ app/dashboard/exam-questions/COC/[subject]/[year]/add-question/page.tsx
@@ -1,5 +1,6 @@
 import { getDepartmentByName } from "@/actions/departments/getDepartmentByName";
 import { getSubjectById } from "@/actions/subject/getSubjectById";
+import { getAllUniversity } from "@/actions/university/getAllUniversity";
 import CreateExamsClient from "@/components/question/createExamQuestion";
 
 
@@ -15,8 +16,9 @@ const AddExitQuestionPage = async({params}:{
 
   const department = await getDepartmentByName("Freshman")
   const subject=await getSubjectById(params.subject)
+  const university=await getAllUniversity();
 
-  return ( <CreateExamsClient isCoc department={department} year={params.year} subject={subject}/> );
+  return ( <CreateExamsClient isCoc={true} university={university} department={department} year={params.year} subject={subject}/> );
 }
  
 export default AddExitQuestionPage;
