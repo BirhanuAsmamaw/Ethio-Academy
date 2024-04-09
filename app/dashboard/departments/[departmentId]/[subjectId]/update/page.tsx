d2fccac537052c87f4f0f5aa3e7a4959e832b512
deribewsoftware <deribewsoftware@gmail.com>
2024-04-09 19:14:44 +0300

    updated

--- app/dashboard/departments/[departmentId]/[subjectId]/update/page.tsx
+++ app/dashboard/departments/[departmentId]/[subjectId]/update/page.tsx
@@ -2,8 +2,8 @@ import { getSubjectById } from '@/actions/subject/getSubjectById'
 import EditSubjectComponent from '@/components/edit-subject/EditSubject'
 import React from 'react'
 
-const UpdateSubjectPage = async({params}:{params:{subject:string}}) => {
-  const subject =await getSubjectById(params.subject)
+const UpdateSubjectPage = async({params}:{params:{subjectId:string}}) => {
+  const subject =await getSubjectById(params.subjectId)
   return (<EditSubjectComponent subject={subject}/>
   )
 }
