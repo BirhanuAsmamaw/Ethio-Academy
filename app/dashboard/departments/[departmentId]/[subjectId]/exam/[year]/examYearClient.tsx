04042723d4ff7b15befa1e85c41530b134c3295e
deribewsoftware <deribewsoftware@gmail.com>
2024-04-09 22:17:08 +0300

    updated

--- app/dashboard/departments/[departmentId]/[subjectId]/exam/[year]/examYearClient.tsx
+++ app/dashboard/departments/[departmentId]/[subjectId]/exam/[year]/examYearClient.tsx
@@ -166,14 +166,14 @@ export const columns: ColumnDef<QuestionType>[] = [
             <DropdownMenuItem> 
               <Link
                className="no-underline text-sm text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200" 
-               href={`/dashboard/exam-questions/EUEE/${question?.subject?.di}/${question.year}/${question.id}/edit`}>Edit question</Link>
+               href={`/dashboard/exam-questions/EUEE/${question?.subject?question?.subject?.id:''}/${question.year}/${question.id}/edit`}>Edit question</Link>
             </DropdownMenuItem>
 
            
             <DropdownMenuItem> 
               <Link 
               className="no-underline text-sm text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200" 
-              href={`/dashboard/exam-questions/EUEE/${question.subject?.id}/${question.year}/${question.id}/delete`}>Delete</Link>
+              href={`/dashboard/exam-questions/EUEE/${question?.subject?question?.subject?.id:''}/${question.year}/${question.id}/delete`}>Delete</Link>
               </DropdownMenuItem>?
           </DropdownMenuContent>
 
