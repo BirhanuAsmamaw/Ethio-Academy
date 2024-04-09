33ab74af17e865748959bcd47a5a034b299d1732
deribewsoftware <deribewsoftware@gmail.com>
2024-04-09 16:56:02 +0300

    updated

--- app/dashboard/departments/[departmentId]/[subjectId]/page.tsx
+++ app/dashboard/departments/[departmentId]/[subjectId]/page.tsx
@@ -31,7 +31,7 @@ const ExamSubjectPage = async({params}:{params:{subjectId:string,departmentId:st
      </div>
 
 
-{subject?.course?<div className="flex justify-center p-4">
+{subject&&subject?.course?<div className="flex justify-center p-4">
    <div className="w-full gap-4 lg:w-10/12 xl:w-8/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     {subject?.course?.map((course:any,index:number)=>{
      return  <ExamsCategoryCard
