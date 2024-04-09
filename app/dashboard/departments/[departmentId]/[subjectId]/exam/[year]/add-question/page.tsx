5ab390768700bae7501e221ad78e06cd486093aa
deribewsoftware <deribewsoftware@gmail.com>
2024-04-09 20:55:04 +0300

    updated

--- app/dashboard/departments/[departmentId]/[subjectId]/exam/[year]/add-question/page.tsx
+++ app/dashboard/departments/[departmentId]/[subjectId]/exam/[year]/add-question/page.tsx
@@ -1,4 +1,5 @@
 import { getDepartmentById } from "@/actions/departments/getDepartmentById";
+import { getSubjectById } from "@/actions/subject/getSubjectById";
 import { getAllUniversity } from "@/actions/university/getAllUniversity";
 import CreateExamsClient from "@/components/question/createExamQuestion";
 
@@ -8,16 +9,18 @@ import CreateExamsClient from "@/components/question/createExamQuestion";
 const AddExitQuestionPage = async({params}:{
   params:{
    departmentId:string,
-    year:string
+    year:string,
+    subjectId:string,
   }
 }) => {
 
   const department = await getDepartmentById(params.departmentId);
   const university=await getAllUniversity();
+  const subject = await getSubjectById(params.subjectId);
 
 
 
-  return ( <CreateExamsClient department={department} university={university} year={params.year}/> );
+  return ( <CreateExamsClient subject={subject} department={department} university={university} year={params.year}/> );
 }
  
 export default AddExitQuestionPage;
