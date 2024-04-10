a2f5fe0d3a8c0c7c608eb3a712d2f7f67512f7ab
deribewsoftware <deribewsoftware@gmail.com>
2024-04-10 11:16:53 +0300

    updated

--- components/card/subjectCard.tsx
+++ components/card/subjectCard.tsx
@@ -10,15 +10,15 @@ interface SubjectCardProps{
 const SubjectCard:React.FC<SubjectCardProps> = ({name,url,image}) => {
   return ( <Link href={url}  className="z-30 no-underline block w-full md:max-w-sm  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 group">
     
-  <div className="flex justify-around gap-2">
-    <div className="">
+  <div className="flex gap-4 md:gap-6">
+    <div className="h-full">
     <Image 
     height={120} 
     width={100} 
     src={image} 
     alt={name}/>
     </div>
-    <div className="">
+    <div className="flex flex-col justify-center h-full justify-center">
     
     <p className=" w-full text-base text-gray-700 font-semibold  dark:text-gray-400 group-hover:dark:text-white ">{name}</p>
     </div>
