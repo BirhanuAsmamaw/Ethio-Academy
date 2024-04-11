83016c2d37bdd93eac3f7d9ba8aa2f02c6e69e14
deribewsoftware <deribewsoftware@gmail.com>
2024-04-11 11:49:44 +0300

    updated

--- app/category/[category]/[subjectId]/page.tsx
+++ app/category/[category]/[subjectId]/page.tsx
@@ -10,6 +10,7 @@ import Card from "@/components/card/card";
 import Navbar from "@/components/navbar/Navbar";
 import PaginationComponent from "@/components/pagination";
 import Spinning from "@/components/spinning";
+import Link from "next/link";
 
 
 
@@ -37,21 +38,23 @@ const Category =async ({params,searchParams}:{params:IPrams,searchParams?:{ [key
 
 <div className="p-4 md:p-6 lg:p-10 xl:p-20 w-full">
      <Banner title={`${department?.departmentName} Courses`}>
-      <h6 className="text-lg font-semibold">{subject?.subjectName}</h6>
+    <div className="flex justify-center w-full">
+    <Link href={`/category${department?.url}`} className=" no-underline hover:underline text-xl md:text-2xl lg:3text-xl xl:text-4xl font-semibold">[{subject?.subjectName}]</Link>
+    </div>
      </Banner>
      </div>
 
    
 
-   {/* Courses */}
-   <div id="courseslist" className=" min-h-screen flex flex-col gap-10 overflow-hidden">
+     {/* Courses */}
+     <div id="coursesubject" className=" min-h-screen flex flex-col w-full gap-10 overflow-hidden">
 
 
 {selectedCourses.length?<div className="flex justify-center w-full">
-<div className="w-full lg:w-11/12 xl:px-20 space-y-4">
+<div className="w-full lg:w-11/12  space-y-4">
   <h1 className='w-full text-xl md:text-4xl font-semibold border-b-2 border-double  p-2 dark:text-gray-300 border-gray-200 dark:border-gray-700 pl-4'>Most common Courses</h1>
 
-  <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-4 p-4 md:px-10'>
+  <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-4 p-4 md:px-10'>
 
     {selectedCourses?.map((course)=>{
       return course.cover&&<Card
@@ -70,8 +73,8 @@ const Category =async ({params,searchParams}:{params:IPrams,searchParams?:{ [key
 
 
   </div>
-  {selectedCourses?.length||0>4?<div className="w-full flex p-4 justify-end">
-    <PaginationComponent paginationLength={selectedCourses.length||0} page={searchParams?.page||'1'} pageUrl='page'/>
+  {selectedCourses?.length>4?<div className="w-full flex p-4 justify-end">
+    <PaginationComponent paginationLength={selectedCourses?.length||0} page={searchParams?.page||'1'} pageUrl='page'/>
   </div>:""}
 </div>
 </div>:""}
@@ -80,9 +83,9 @@ const Category =async ({params,searchParams}:{params:IPrams,searchParams?:{ [key
 
 
 {newSelectedCourses?.length?<div className="flex justify-center w-full ">
-<div className="w-full lg:w-11/12 space-y-4 xl:px-20">
+<div className="w-full lg:w-11/12 space-y-4 ">
 <h1 className='w-full text-xl md:text-4xl font-semibold border-b-2 border-double p-2 dark:text-gray-300 border-gray-200 dark:border-gray-700 pl-4'>New Coming  Courses</h1>
-<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-4 p-4 md:px-10'>
+<div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-center flex-wrap gap-4 p-4 md:px-10'>
 
     {newSelectedCourses?.map((course,index)=>{
       return course.cover&&<Card
@@ -100,9 +103,11 @@ const Category =async ({params,searchParams}:{params:IPrams,searchParams?:{ [key
 
 
   </div>
-  {newSelectedCourses?.length||0>4?<div className="w-full flex p-4 justify-end">
-    <PaginationComponent paginationLength={ newSelectedCourses?.length||0} page={searchParams?.newpage||'1'} pageUrl='newpage'/>
+  {newSelectedCourses?.length>4?<div className="w-full flex p-4 justify-end">
+    <PaginationComponent paginationLength={newSelectedCourses?.length||0} page={searchParams?.newpage||'1'} pageUrl='newpage'/>
   </div>:""}
+
+  
 </div>
 </div>:""}
 
