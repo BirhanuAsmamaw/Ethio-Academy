a8be6cf03f5c9a7e4ea01d6fe4283669e761d878
deribewsoftware <deribewsoftware@gmail.com>
2024-04-11 15:30:12 +0300

    updated

--- components/question/createExamQuestion.tsx
+++ components/question/createExamQuestion.tsx
@@ -105,7 +105,7 @@ console.log("data",qData);
   axios.post('/api/question',qData).then(()=>{
     toast.success("Question created successfully")
     
-    router.push(backUrl||`/dashboard/departments/${department?.id}/${subject?.id}`);
+    router.push(backUrl||`/dashboard/departments/${department?.id}/${subject?.id}/exam/${year}`);
     router.refresh();
   }).catch((error:any)=>{
    
