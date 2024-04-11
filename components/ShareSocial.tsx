7d0433688a3c5b545e67b1916ed3822943b14863
deribewsoftware <deribewsoftware@gmail.com>
2024-04-11 12:09:22 +0300

    updated

--- components/ShareSocial.tsx
+++ components/ShareSocial.tsx
@@ -47,26 +47,26 @@ const ShareSocialMedia:React.FC<CustomShareButtonProps> = ({url}) => {
         </div>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[325px] bg-white dark:bg-gray-700 p-2 font-bold rounded-[10px]">
-        <div className="flex flex-col gap-2 ">
+        <div className="flex flex-wrap gap-2 justify-center ">
         <TelegramShareButton url={url}>
-        <TelegramIcon size={40} round={true} />
+        <TelegramIcon size={30} round={true} />
       </TelegramShareButton>
 
       <FacebookShareButton url={url}>
-        <FacebookIcon size={40} round={true} />
+        <FacebookIcon size={30} round={true} />
       </FacebookShareButton>
       <LinkedinShareButton url={url}>
-        <LinkedinIcon size={40} round={true} />
+        <LinkedinIcon size={30} round={true} />
       </LinkedinShareButton>
 
 
       <TwitterShareButton url={url}>
-        <TwitterIcon size={40} round={true} />
+        <TwitterIcon size={30} round={true} />
       </TwitterShareButton>
 
 
       <WhatsappShareButton url={url}>
-        <WhatsappIcon size={40} round={true} />
+        <WhatsappIcon size={30} round={true} />
       </WhatsappShareButton>
 
         </div>
