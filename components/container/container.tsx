b54cb9535c387d2118fb8fdc4838ed1975b2ffa9
deribewsoftware <deribewsoftware@gmail.com>
2024-04-13 09:37:04 +0300

    add animation on container

--- components/container/container.tsx
+++ components/container/container.tsx
@@ -1,13 +1,18 @@
 import React, { ReactNode } from "react";
-
+import {motion} from "framer-motion"
 interface ContainerProps{
   childern: ReactNode;
 }
 
 const Container:React.FC<ContainerProps> = ({childern}) => {
-  return ( <div className="bg-white dark:bg-gray-800 border-slate-200 dark:border-gray-600 border-[1.5px] rounded-[5px]">
+  return ( <motion.div 
+    initial={{opacity: 0,y:15}}
+    whileInView={{opacity:1,y:0}}
+    exit={{opacity:0,y:15}}
+    transition={{delay:0.25}}
+  className="bg-white dark:bg-gray-800 border-slate-200 dark:border-gray-600 border-[1.5px] rounded-[5px]">
     {childern}
-  </div> );
+  </motion.div> );
 }
  
 export default Container;
