86904dc14e95e8e66e70671a4500ec0f841ea117
deribewsoftware <deribewsoftware@gmail.com>
2024-04-14 17:21:26 +0300

    updated

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,11 @@
+import ScaleLoading from '@/components/scaleLoading'
 import React from 'react'
 
-import { ScaleLoader } from 'react-spinners'
 const LoadingPage = () => {
   return (<div className=' h-screen w-full flex justify-center items-center'>
 
     <div className="">
-    <ScaleLoader color="#36d7b7" />
+      <ScaleLoading/>
     </div>
   </div>
   )
---
+++ components/scaleLoading.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import { ScaleLoader } from 'react-spinners'
+
+const ScaleLoading = () => {
+  return ( <ScaleLoader color="#36d7b7" />
+  )
+}
+
+export default ScaleLoading
