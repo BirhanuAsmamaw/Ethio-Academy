61231991b8b3f05f4906b86f22a73ffe76e6a1d3
deribewsoftware <deribewsoftware@gmail.com>
2024-04-17 01:27:30 +0300

    updated

--- app/dashboard/departments/[departmentId]/[subjectId]/course/[courseId]/chapter/createLesson.tsx
+++ app/dashboard/departments/[departmentId]/[subjectId]/course/[courseId]/chapter/createLesson.tsx
@@ -15,9 +15,10 @@ import { useRouter } from "next/navigation";
 import {  useState } from "react";
 import { FieldValues, SubmitHandler,  useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-import { IoIosSend } from "react-icons/io";
+import { IoIosSend, IoMdAdd } from "react-icons/io";
 import { Button } from "@/components/ui/button";
 import AddButton from "@/components/button/addButton";
+import { motion } from "framer-motion";
 interface LessonProps{
   chapterId: string;
 }
@@ -78,7 +79,19 @@ const router=useRouter();
 
   return ( <Dialog>
       <DialogTrigger asChild>
-      <AddButton label="Add Lesson"/> 
+      <motion.button 
+    
+    initial={{scale: 1}}
+    whileHover={{scale:1.1}}
+    whileTap={{scale:1}}
+    transition={{duration:0.25}}
+    className="py-2 md:py-2.5 px-3 md:px-5 me-2 mb-2
+text-sm font-medium text-gray-900 focus:outline-none
+ bg-white rounded-full border border-gray-200 
+ hover:bg-gray-100 hover:text-blue-700 focus:z-10 
+ focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700
+  dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600
+   dark:hover:text-white dark:hover:bg-gray-700 flex gap-2 items-center justify-center"><IoMdAdd size={24}/> <p>Lesson</p></motion.button> 
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
