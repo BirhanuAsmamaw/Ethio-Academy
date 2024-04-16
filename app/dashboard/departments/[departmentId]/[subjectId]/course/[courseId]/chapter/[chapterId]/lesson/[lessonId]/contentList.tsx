9ce5cf10d75d2b534c75e05982b3d0ef7e87dc94
deribewsoftware <deribewsoftware@gmail.com>
2024-04-16 20:28:03 +0300

    updated

--- app/dashboard/departments/[departmentId]/[subjectId]/course/[courseId]/chapter/[chapterId]/lesson/[lessonId]/contentList.tsx
+++ app/dashboard/departments/[departmentId]/[subjectId]/course/[courseId]/chapter/[chapterId]/lesson/[lessonId]/contentList.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 
@@ -10,7 +11,14 @@ const ContentList:React.FC<ContentListProps> = ({lesson}) => {
   }
   return (<div className='flex flex-col gap-4'>
    {lesson?.contents?.map((content:any) =>{
-    return  <div key={content?.id} className="p-4 hover:border" dangerouslySetInnerHTML={{__html:content?.content}}></div>
+    return  <div  key={content?.id} className="p-4 relative w-full bg-white dark:bg-gray-800 hover:border-2 hover:border-sky-400 hover:border-dashed  transition duration-300 group" dangerouslySetInnerHTML={{__html:content?.content}}>
+      <div className="hidden  group-hover:block  absolute left-[40%] top-[50%]  w-full">
+ <div className="flex justify-center gap-10">
+<Button>Edit</Button>
+<Button>Delete</Button>
+ </div>
+      </div>
+    </div>
    })}
   </div>
  
