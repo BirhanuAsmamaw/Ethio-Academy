34b5ebaad506c5325c9f574289c5050aa6cb3950
deribewsoftware <deribewsoftware@gmail.com>
2024-04-20 10:12:20 +0300

    updated

--- app/dashboard/profile/profileClient.tsx
+++ app/dashboard/profile/profileClient.tsx
@@ -26,22 +26,30 @@ const ProfileClient:React.FC<ProfileClientProps> = ({user}) => {
   
 
 
-  return ( <Tabs defaultValue="account" className=" shadow-lg dark:shadow-black border dark:border-gray-700 rounded-[10px] w-full ">
-  <TabsList className="grid w-full grid-cols-2 gap-4 bg-white dark:bg-black p-2 ">
-    <TabsTrigger className="rounded-full" value="account">Account</TabsTrigger>
-    <TabsTrigger className="rounded-full"  value="password">Password</TabsTrigger>
+  return ( <Tabs defaultValue="account" className=" w-full ">
+  <TabsList className=" flex flex-wrap w-full gap-4 p-2 ">
+    <TabsTrigger className="rounded-full bg-slate-100 dark:bg-gray-800  data-[state=active]:text-white data-[state=active]:bg-[#4C6FFF]" value="account">Account</TabsTrigger>
+    <TabsTrigger className="rounded-full bg-slate-100 dark:bg-gray-800  data-[state=active]:text-white data-[state=active]:bg-[#4C6FFF]"  value="password">Password</TabsTrigger>
+    <TabsTrigger className="rounded-full bg-slate-100 dark:bg-gray-800 data-[state=active]:text-white data-[state=active]:bg-[#4C6FFF]"  value="teacherForm">Teacher Form</TabsTrigger>
   </TabsList>
 
-  <TabsContent value="account" className="flex justify-center">
+  <TabsContent value="account" className="flex justify-center mt-6">
   <EditAccount user={user}/>
   </TabsContent>
 
 
 
-  <TabsContent value="password" className="flex justify-center">
+  <TabsContent value="password" className="flex justify-center mt-6">
   <EditPassword user={user}/>
   </TabsContent>
 
+
+
+  <TabsContent value="teacherForm" className="flex justify-center mt-6">
+ <div className="form">
+  <h1>teacher form</h1>
+ </div>
+  </TabsContent>
 
 </Tabs> );
 }
--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex border dark:border-gray-600  items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background text-gray-500 dark:text-gray-400 transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-b-2 data-[state=active]:border-green-400 data-[state=active]:dark:border-green-400 data-[state=active]:text-green-500  data-[state=active]:dark:text-green-400 font-semibold data-[state=active]:shadow-sm",
+      "inline-flex border dark:border-gray-600  items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm  ring-offset-background text-gray-500 dark:text-gray-400 transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-b-2 data-[state=active]:border-green-400 data-[state=active]:dark:border-green-400 data-[state=active]:text-green-500  data-[state=active]:dark:text-green-400 font-semibold data-[state=active]:shadow-sm",
       className
     )}
     {...props}
