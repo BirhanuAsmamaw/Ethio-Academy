5348310739497579d9e51e160f24a54914c15547
deribewsoftware <deribewsoftware@gmail.com>
2024-04-21 16:28:42 +0300

    updated

--- components/ChannelVerfiedComponent.tsx
+++ components/ChannelVerfiedComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { MdVerified } from "react-icons/md";
 const ChannelVerfiedComponent = () => {
-  return <MdVerified className=' text-amber-300 bg-gray-800 overflow-hidden'/>
+  return <MdVerified className=' text-amber-400 dark:text-amber-300'/>
   
 }
 
