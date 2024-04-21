616245ff5e2825414e81819aa017b2b41e5a1ef3
deribewsoftware <deribewsoftware@gmail.com>
2024-04-21 14:30:39 +0300

    updated

--- app/course/coursedescribeList.tsx
+++ app/course/coursedescribeList.tsx
@@ -89,7 +89,7 @@ const onPayment=()=>{
 <ShareSocialMedia url={`https://ethio-exams-academy.vercel.app/course/${course.id}`}/>
 
 </div>
-    <div className="p-2 flex justify-end gap-6">
+    {course.price?<div className="p-2 flex justify-end gap-6">
       <button 
       onClick={onAddToCart}
       className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
@@ -99,7 +99,7 @@ const onPayment=()=>{
       onClick={onPayment}
       className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
       >Buy Now!!</button>
-    </div>
+    </div>:""}
   <h5 className="px-2 text-lg font-medium text-gray-500 dark:text-gray-400">Course Content</h5>
  <div className="flex flex-col  ">
 
