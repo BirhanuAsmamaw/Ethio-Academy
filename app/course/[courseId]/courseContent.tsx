a1e7f26ab641b38d8ebe5d611ec44359bf74af4e
deribewsoftware <deribewsoftware@gmail.com>
2024-04-22 23:49:36 +0300

    updated

--- app/course/[courseId]/courseContent.tsx
+++ app/course/[courseId]/courseContent.tsx
@@ -33,13 +33,12 @@ router.refresh()
     <Accordion type="single" collapsible className="w-full p-1" >
 
       {course.chapters.map((chapter:any,index:number) =>{
-        return <AccordionItem key={index} value={`${index}`}
-        className="border bg-white dark:bg-black  border-slate-200 dark:border-gray-600 lg:px-6 lg:m-2 rounded-[6px] ">
-        <AccordionTrigger className="hover:no-underline" ><div className="flex gap-2  text-captalize  ">
+        return <AccordionItem key={index} value={`${index}`}className="border bg-background   w-full  border-slate-200 dark:border-gray-600 px-2 md:px-6 m-2 rounded-[6px] ">
+        <AccordionTrigger className="hover:no-underline w-full" ><div className="flex gap-2  w-full text-captalize  ">
          <p className="h-5 w-5 rounded-full bg-green-400  items-center text-center text-sm text-black">{index+1}</p>
          <p className="text-base">{chapter.title}</p>
           </div></AccordionTrigger>
-        <AccordionContent className="bg-background ">
+        <AccordionContent className="bg-background w-full">
           <ul className="">
             {chapter.lessons.map((lesson:any,ind:number)=>{
 return <li onClick={()=>onLessonRouter(course.id,`${chapter.id}`,`${lesson.id}`)} className="flex text-sm gap-2  hover:underline  hover:text-teal-500  transition duration-300" key={ind}>
