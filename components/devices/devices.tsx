b3e825a7f19cf619706545d2e30be10261c2913b
deribewsoftware <deribewsoftware@gmail.com>
2024-04-26 01:40:04 +0300

    updated

--- components/devices/devices.tsx
+++ components/devices/devices.tsx
@@ -7,8 +7,8 @@ const DevicesComponent = () => {
   return (<div className="dark:bg-black p-10 md:p-20 bg-sky-100">
     <h1 className='text-xl lg:text-3xl leading-6 p-4 font-semibold'>Our website Access Easily By these devices</h1>
 
-  <div className="w-full md:w-10/12 lg:w-8/12 xl:w-1/2 flex justify-center">
-  <div className="flex flex-wrap  justify-center gap-10">
+  <div className="w-full  flex justify-center">
+  <div className="flex flex-wrap  justify-center gap-10 w-full md:w-10/12 lg:w-8/12 ">
         <MobileComponent/>
         <TabletComponent/>
         <PcComponent/>
