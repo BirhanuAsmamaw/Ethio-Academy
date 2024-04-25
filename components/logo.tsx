477b59e1298a74f0e039df55090f4d4f5cbecb8b
deribewsoftware <deribewsoftware@gmail.com>
2024-04-26 02:18:42 +0300

    updated

--- components/logo.tsx
+++ components/logo.tsx
@@ -8,8 +8,8 @@ const Logo = () => {
   return (
     <Link href="/" className='  no-underline hover:bg-gray-100  hover:underline'><div className={`text-xl flex  gap-2 lg:font-extrabold ${Oscript.className}`}>
     <div className="  text-blue-500 dark:text-green-400  lg:hidden rounded-full drop-shadow-2xl text-4xl">EEA</div>
-    <div className={`text-blue-500 dark:text-green-400  hidden lg:block leading-10 drop-shadow-2xl ${lugrasimo.className}`}>
-        <h1>Ethio Exams Academy</h1>
+    <div className={`text-blue-500 dark:text-green-400   hidden lg:block leading-10 drop-shadow-2xl ${lugrasimo.className}`}>
+        <h1 className='text-2xl'>Ethio Exams Academy</h1>
     </div>
 </div></Link>
   )
