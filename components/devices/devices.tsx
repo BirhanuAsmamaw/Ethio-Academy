0e6d36a60ab1c38f4884feeb0cd28ad3cd86549b
deribewsoftware <deribewsoftware@gmail.com>
2024-04-26 01:33:29 +0300

    updated

--- components/devices/devices.tsx
+++ components/devices/devices.tsx
@@ -4,14 +4,16 @@ import TabletComponent from './tablet'
 import PcComponent from './pc'
 
 const DevicesComponent = () => {
-  return (<div className="dark:bg-black p-10 md:p-20 bg-slate-200">
-    <h1 className='text-xl leading-6 p-4 font-semibold'>Our website Access Easily By these devices</h1>
+  return (<div className="dark:bg-black p-10 md:p-20 bg-sky-100">
+    <h1 className='text-xl lg:text-3xl leading-6 p-4 font-semibold'>Our website Access Easily By these devices</h1>
 
-    <div className="flex flex-wrap justify-center ">
+  <div className="w-full md:w-10/12 lg:w-8/12 xl:w-1/2 flex justify-center">
+  <div className="flex flex-wrap  justify-center gap-10">
         <MobileComponent/>
         <TabletComponent/>
         <PcComponent/>
     </div>
+  </div>
 
   </div>
     
