a57eafbd1bd71a0e74c14ca0173aeacb1ac8f88e
deribewsoftware <deribewsoftware@gmail.com>
2024-04-26 02:13:06 +0300

    updated

--- components/logo.tsx
+++ components/logo.tsx
@@ -6,7 +6,7 @@ const lugrasimo=Lugrasimo({style:["normal"],subsets:["latin"],weight:["400"]} )
 const Oscript=Oleo_Script({style:["normal"],subsets:["latin"],weight:["400"]} )
 const Logo = () => {
   return (
-    <Link href="/" className='  no-underline hover:bg-gray-100  hover:underline'><div className={`text-xl lg:text-3xl flex  gap-2 lg:font-extrabold ${Oscript.className}`}>
+    <Link href="/" className='  no-underline hover:bg-gray-100  hover:underline'><div className={`text-xl flex  gap-2 lg:font-extrabold ${Oscript.className}`}>
     <div className="  text-blue-500 dark:text-green-400  lg:hidden rounded-full drop-shadow-2xl text-4xl">EEA</div>
     <div className={`text-blue-500 dark:text-green-400  hidden lg:block leading-10 drop-shadow-2xl ${lugrasimo.className}`}>
         <h1>Ethio Exams Academy</h1>
