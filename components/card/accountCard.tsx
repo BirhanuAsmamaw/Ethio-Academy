cadd128cee3362518148eb41ff5ff136f8c0ffde
Birhanu Asmamaw <birhanuasmamaw24@gmail.com>
2024-05-26 12:54:14 -0700

    updated

--- components/card/accountCard.tsx
+++ components/card/accountCard.tsx
@@ -20,7 +20,7 @@ const AccountCard:React.FC<AccountCardProps>= ({url,image,name,title,learner_no,
   return (
     <Link href={url} className='bg-white no-underline shadow border dark:border-gray-500 hover:border-green-500 hover:dark:border-yellow-400 hover:bg-slate-50 hover:dark:bg-gray-700 dark:bg-gray-800 max-w-64 overflow-hidden p-2 h-fit w-fit flex justify-center rounded-lg transition duration-300'>
       <div className="flex flex-col  justify-center text-center items-center">
-      <Avatar className={`${!image&&'hidden'}  h-16 w-16 `}>
+      <Avatar className={`  h-16 w-16 `}>
       <AvatarImage src={image||""} alt="image" />
       <AvatarFallback>{names? names[0][0]:''}{names?names[1][0]:''}</AvatarFallback>
     </Avatar>
--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,6 +1,7 @@
 
 import Link from 'next/link';
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { CiLinkedin } from "react-icons/ci";
+import { FaTelegramPlane } from "react-icons/fa";
 import Logo from '../logo';
 const Footer = () => {
   return ( 
@@ -14,9 +15,30 @@ const Footer = () => {
               <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                   
                   <div>
+                      <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Exam</h2>
+                      <ul className="text-gray-500 list-none dark:text-gray-400 text-sm ">
+                          <li className="">
+                              <Link href="/exams/Exit" className="no-underline text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200  hover:underline">Univerity Exit Exams (Exit) </Link>
+                          </li>
+                          <li>
+                              <Link  href="/exams/EUEE"  className="no-underline text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200  hover:underline">University Entrance Exams (EUEE)</Link>
+                          </li>
+
+                          <li>
+                              <Link  href="/exams/Remedial"  className="no-underline text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200  hover:underline">Remedial Exams</Link>
+                          </li>
+
+
+                          <li>
+                              <Link  href="/exams/COC"  className="no-underline text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200  hover:underline">COC Exams</Link>
+                          </li>
+                      </ul>
+                  </div>
+
+                  <div>
                       <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                      <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                          <li className="mb-4">
+                      <ul className="text-gray-500 text-sm list-none dark:text-gray-400 ">
+                          <li className="">
                               <Link href="/privancy-policy" className="no-underline text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200  hover:underline">Privacy Policy </Link>
                           </li>
                           <li>
@@ -24,10 +46,23 @@ const Footer = () => {
                           </li>
                       </ul>
                   </div>
+
+
+                  <div>
+                      <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow Us</h2>
+                      <ul className="text-gray-500 text-sm list-none dark:text-gray-400 ">
+                          <li className="">
+                              <Link href="https://www.linkedin.com/in/deribew-shimelis-182895297" className=" flex items-center gap-x-2 no-underline text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200  hover:underline"><CiLinkedin size={20}/> <span>Linkedin</span> </Link>
+                          </li>
+                          <li className="">
+                              <Link href="https://t.me/Derishemi" className=" flex items-center gap-x-2 no-underline text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200  hover:underline"><FaTelegramPlane size={20}/> <span>Telegram</span> </Link>
+                          </li>
+                      </ul>
+                  </div>
               </div>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="sm:flex sm:items-center sm:justify-center">
               <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link href="/" className="no-underline text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200  hover:underline">Ethio Exams Academy™</Link>. All Rights Reserved.
               </span>
              
